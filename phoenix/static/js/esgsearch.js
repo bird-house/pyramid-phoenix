46e617e00cf29ece2ee34b1b87b12a50b4e629b4
Carsten Ehbrecht <ehbrecht@dkrz.de>
2013-08-12 18:10:36 +0200

    simplified esgsearch.js

--- phoenix/static/js/esgsearch.js
+++ phoenix/static/js/esgsearch.js
@@ -1,22 +1,52 @@
+var init_esgsearch = function(oid) {
+  jQuery(".tm-selection").tagsManager({
+    prefilled: ["variable:hus", "experiment:decadal1960"],
+    preventSubmitOnEnter: true,
+    delimiters: [9, 13, 44],
+    maxTags: 2,
+    tagClass: 'tm-tag tm-tag-success',
+    hiddenTagListId: oid,
+  });
+
+  jQuery(".tm-facets").tagsManager({
+    //prefilled: ["hello"],
+    preventSubmitOnEnter: true,
+    delimiters: [9, 13, 44],
+    //maxTags: 1,
+    tagClass: 'tm-tag tm-tag-info',
+    tagCloseIcon: '+',
+  });
+
+  jQuery(".tm-facet").tagsManager({
+    //prefilled: ["MPI-M", "NCC", "MIROC", "BCC"],
+    preventSubmitOnEnter: true,
+    delimiters: [9, 13, 44],
+    //maxTags: 4,
+    tagClass: 'tm-tag tm-tag-warning tm-tag-mini',
+    tagCloseIcon: '+',
+  });
+};
+
+
 (function($) {
-  
+
+  "use strict";
+
   $.esgsearch = function() {
     var url = 'http://adelie.d.dkrz.de:8090/esg-search/search?limit=0&distrib=false&facets=*&format=application%2Fsolr%2Bjson'
-    var tags = [];
     $.getJSON(url, function(json) {
-      tags = json.responseHeader.params['facet.field'];
+      var tags = json.responseHeader.params['facet.field'];
       $.each(tags, function(i, tag) {
 	jQuery(".tm-facets").tagsManager('pushTag', tag);
       });
-      counts = json.facet_counts.facet_fields['institute'];
+      var counts = json.facet_counts.facet_fields['institute'];
       $.each(counts, function(i,value) {
 	if (i % 2 == 0) {
 	  jQuery(".tm-facet").tagsManager('pushTag', value);
 	}
       });
-
+      
     });
-    return tags;
   }
 
 })(jQuery);
