fa7c1a8a43ebddbc00eecb17f06a81a35e620aa7
Carsten Ehbrecht <carsten@localhost>
2013-08-17 16:24:54 +0200

    refactoring esgsearch.js as jquery plugin

--- phoenix/static/js/esgsearch.js
+++ phoenix/static/js/esgsearch.js
@@ -1,77 +1,97 @@
-var selected_facet_handler = function (facet) {
-  alert('facet = ' + facet);
-};
-
-var selected_facet_value_handler = function (facet_value) {
-  alert('facet_value = ' + facet_value);
-};
-
-var init_esgsearch = function(oid) {
-  jQuery(".tm-selection").tagsManager({
-    prefilled: ["experiment:decadal1960"],
-    preventSubmitOnEnter: true,
-    delimiters: [9, 13, 44],
-    //maxTags: 2,
-    tagClass: 'tm-tag tm-tag-success',
-    hiddenTagListId: oid,
-  });
-
-  jQuery(".tm-facets").tagsManager({
-    //prefilled: ["hello"],
-    preventSubmitOnEnter: true,
-    delimiters: [9, 13, 44],
-    //maxTags: 1,
-    tagClass: 'tm-tag tm-tag-info',
-    isSelectable: true,
-    selectHandler: selected_facet_handler,
-  });
-
-  jQuery(".tm-facet").tagsManager({
-    //prefilled: ["MPI-M", "NCC", "MIROC", "BCC"],
-    preventSubmitOnEnter: true,
-    delimiters: [9, 13, 44],
-    //maxTags: 4,
-    tagClass: 'tm-tag tm-tag-warning tm-tag-mini',
-    isSelectable: true,
-    selectHandler: selected_facet_value_handler,
-  });
-};
-
-
 (function($) {
+  $.extend({
+    EsgSearch: function(options) {
+      var defaults = {
+	oid: null,
+	url: 'http://tracy.local:8090/esg-search/search',
+	limit: '0',
+	distrib: 'false',
+	format: 'application%2Fsolr%2Bjson',
+      };
+      var searchOptions = $.extend(defaults, options);
 
-  "use strict";
+      var init = function() {
+	init_constraints(searchOptions.oid);
+	init_facets();
+	init_facet_values();
+	execute();
+      };
 
-  var url = 'http://tracy.local:8090/esg-search/search';
-  var limit = '0';
-  var distrib = 'false';
-  var format = 'application%2Fsolr%2Bjson';
+      var selected_facet_handler = function (facet) {
+	alert('facet = ' + facet);
+      };
+
+      var selected_facet_value_handler = function (facet_value) {
+	alert('facet_value = ' + facet_value);
+      };
+
+      var init_constraints = function(oid) {
+	jQuery(".tm-selection").tagsManager({
+	  prefilled: ["experiment:decadal1960"],
+	  preventSubmitOnEnter: true,
+	  delimiters: [9, 13, 44],
+	  //maxTags: 2,
+	  tagClass: 'tm-tag tm-tag-success',
+	  hiddenTagListId: oid,
+	});
+      };
+
+      var init_facets = function() {
+	jQuery(".tm-facets").tagsManager({
+	  //prefilled: ["hello"],
+	  preventSubmitOnEnter: true,
+	  delimiters: [9, 13, 44],
+	  //maxTags: 1,
+	  tagClass: 'tm-tag tm-tag-info',
+	  isSelectable: true,
+	  selectHandler: selected_facet_handler,
+	});
+      };
+
+      var init_facet_values = function() {
+	jQuery(".tm-facet").tagsManager({
+	  //prefilled: ["MPI-M", "NCC", "MIROC", "BCC"],
+	  preventSubmitOnEnter: true,
+	  delimiters: [9, 13, 44],
+	  //maxTags: 4,
+	  tagClass: 'tm-tag tm-tag-warning tm-tag-mini',
+	  isSelectable: true,
+	  selectHandler: selected_facet_value_handler,
+	});
+      };
     
-  $.esgsearch = function() {
-    var constraints = '';
-    var tags = $("#deformField1").val().split(",");
-    $.each(tags, function(i, tag) {
-      var constraint = tag.split(":");
-      constraints += '&' + constraint[0] + '=' + constraint[1];
-    });
+      var execute = function() {
+	var constraints = '';
+	var tags = $("#deformField1").val().split(",");
+	$.each(tags, function(i, tag) {
+	  var constraint = tag.split(":");
+	  constraints += '&' + constraint[0] + '=' + constraint[1];
+	});
 
-    var searchURL = url + '?' + 'facets=*' + constraints + '&limit=' + limit + '&distrib=' + distrib + '&format=' + format;
+	var searchURL = searchOptions.url + '?';
+	searchURL += 'facets=*' + constraints; 
+	searchURL += '&limit=' + searchOptions.limit; 
+	searchURL += '&distrib=' + searchOptions.distrib; 
+	searchURL += '&format=' + searchOptions.format;
    
-    $.getJSON(searchURL, function(json) {
-      var tags = json.responseHeader.params['facet.field'];
-      $.each(tags, function(i, tag) {
-	jQuery(".tm-facets").tagsManager('pushTag', tag);
-      });
-      var counts = json.facet_counts.facet_fields['institute'];
-      $.each(counts, function(i,value) {
-	if (i % 2 == 0) {
-	  jQuery(".tm-facet").tagsManager('pushTag', value);
-	}
-      });
-      
-    });
-  };
+	$.getJSON(searchURL, function(json) {
+	  var tags = json.responseHeader.params['facet.field'];
+	  $.each(tags, function(i, tag) {
+	    jQuery(".tm-facets").tagsManager('pushTag', tag);
+	  });
+	  var counts = json.facet_counts.facet_fields['institute'];
+	  $.each(counts, function(i,value) {
+	    if (i % 2 == 0) {
+	      jQuery(".tm-facet").tagsManager('pushTag', value);
+	    }
+	  });
+	});
+      };
 
+      init();
+    },
+  });
 })(jQuery);
+
 
 
