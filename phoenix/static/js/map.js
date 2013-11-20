1a0102f8ea7e6fadcb4b5cd421695f2ab7552dde
Carsten Ehbrecht <ehbrecht@dkrz.de>
2013-11-20 16:50:13 +0100

    init map from wps layer call

--- phoenix/static/js/map.js
+++ phoenix/static/js/map.js
@@ -1,9 +1,6 @@
 var map, ia_wms, tds_wms;
-
-var timesteps = ["2006-01-01T12:00:00", "2006-01-02T12:00:00", "2006-01-03T12:00:00", 
-                 "2006-01-04T12:00:00", "2006-01-05T12:00:00", "2006-01-06T12:00:00", 
-                 "2006-01-07T12:00:00", "2006-01-08T12:00:00", "2006-01-09T12:00:00", 
-                 "2006-01-10T12:00:00",];
+var wps;
+var timesteps = [];
 
 function init() {
   var mapOptions = { maxResolution: 256/512, numZoomLevels: 11, fractionalZoom: true};
@@ -14,7 +11,7 @@ function init() {
 
   var layer = new OpenLayers.Layer.WMS( "OpenLayers WMS",
                                         "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
-  //map.addLayer(layer);
+  map.addLayer(layer);
 
   // WMS tiled
   layer = new OpenLayers.Layer.WMS("World Map",
@@ -24,30 +21,11 @@ function init() {
                                   );
   layer.setVisibility(true)
   map.addLayer(layer);
-
-
-  // wms-t from tds
-  tds_wms = new OpenLayers.Layer.WMS("TDS Test",
-                                     "http://rockhopper.d.dkrz.de:8090/thredds/wms/test/cordex-eur-tas-pywpsInputT1Zaki.nc?",
-                                     {layers:"tas",
-                                      transparent:true,
-                                      format:'image/png',
-                                      time:"2006-01-01T12:00:00.000Z"});
-
- 
-  
-  map.addLayer(tds_wms);
- 
-  //map.zoomToExtent(new OpenLayers.Bounds(-100.898437,22.148438,-78.398437,39.726563));
   map.zoomToMaxExtent();
+}
 
-  // call wps
-  var wps = new SimpleWPS({
-    url: "http://rockhopper.d.dkrz.de:8090/wps",
-    onSuccess: function(result) {console.log(result)},
-  });
-  wps.execute();
-
+function initUI() {
+  var max = timesteps.length - 1;
 
   $("#prev").button({
     icons: {
@@ -65,7 +43,7 @@ function init() {
   $("#slider").slider({
     value: 0,
     min: 0,
-    max: 9,
+    max: max,
     step: 1,
     slide: function(e, ui) {
       var step = parseInt(ui.value);
@@ -90,7 +68,7 @@ function init() {
   }).click(function( event ) {
     console.log('next button clicked');
     current = $("#slider").slider( "values", 0 );
-    if (current < 9 ) {
+    if (current < max ) {
       $("#slider").slider( "value", current + 1 );
     }
   });
@@ -115,6 +93,34 @@ function init() {
   });
 }
 
+function initWPS() {
+  wps = new SimpleWPS({
+    url: "http://rockhopper.d.dkrz.de:8090/wps",
+    process: "org.malleefowl.wms.layer",
+    onSuccess: function(result) {
+      console.log(result);
+      layer = result[0]
+      timesteps = layer.timesteps;
+      // wms-t from tds
+      tds_wms = new OpenLayers.Layer.WMS(layer.title,
+                                         "http://rockhopper.d.dkrz.de:8090/thredds/wms/test/cordex-eur-tas-pywpsInputT1Zaki.nc?",
+                                         {layers: layer.name,
+                                          transparent: true,
+                                          format:'image/png',
+                                          time: layer.timesteps[0]});
+      
+ 
+  
+      map.addLayer(tds_wms);
+ 
+      //map.zoomToExtent(new OpenLayers.Bounds(-100.898437,22.148438,-78.398437,39.726563));
+      map.zoomToMaxExtent();
+
+      initUI();
+    },
+  });
+}
+
 function animate(step) {
   if (step < 10) {
     $("#slider").slider( "value", step );
@@ -124,4 +130,7 @@ function animate(step) {
 
 $(document).ready(function (e) {
   init();
+  //initUI();
+  initWPS();
+  wps.execute();
 });
