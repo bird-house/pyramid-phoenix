148d2b770ddecbf9e92680957468e81062c478c4
Carsten Ehbrecht <ehbrecht@dkrz.de>
2013-11-21 18:21:40 +0100

    using layer selector

--- phoenix/static/js/map.js
+++ phoenix/static/js/map.js
@@ -1,15 +1,18 @@
-var map, animate_layer;
+var map,  wmsLayer, animateLayer;
 var start_time, end_time;
+var layerList;
 
 function initMap() {
   var mapOptions = { maxResolution: 256/512, numZoomLevels: 11, fractionalZoom: true};
   //map = new OpenLayers.Map('map',mapOptions);
   map = new OpenLayers.Map('map');
   map.addControl(new OpenLayers.Control.LayerSwitcher());
+  //map.addControl(new OpenLayers.Control.Attribution());
   //map.addControl(new OpenLayers.Control.Animate());
 
   var layer = new OpenLayers.Layer.WMS( "OpenLayers WMS",
-                                        "http://vmap0.tiles.osgeo.org/wms/vmap0", {layers: 'basic'} );
+                                        "http://vmap0.tiles.osgeo.org/wms/vmap0", 
+                                        {layers: 'basic'} );
   map.addLayer(layer);
 
   // WMS tiled
@@ -21,40 +24,55 @@ function initMap() {
   map.addLayer(layer);
   map.zoomToMaxExtent(); 
 
-  initLayer();
+  initLayerList();
 }
 
-function initLayer() {
+function initLayerList() {
   wps = new SimpleWPS({
     process: "org.malleefowl.wms.layer",
     onSuccess: function(result) {
-      console.log(result);
+      //console.log(result);
+      var output = [];
+      layerList = []
       $.each(result, function(index, layer) {
-        wms_layer = new OpenLayers.Layer.WMS(layer.service_name + ":" + layer.title,
-                                             layer.service,
-                                             {layers: layer.name,
-                                              transparent: true,
-                                              format:'image/png',
-                                              time: layer.timesteps[0]});
-      
- 
-  
-        wms_layer.setVisibility(false);
-        map.addLayer(wms_layer);
-        layer.wms_layer = wms_layer;
-        wms_layer.events.register("visibilitychanged", layer, function() {
-          if (layer.wms_layer.getVisibility()) {
-            initTimeSlider(this);
-            initRangeSlider(this);
-          }
-        });
+        //output.push('<optgroup label="' + layer.service_name + '">');
+        title = layer.title + " (" + layer.service_name + ")";
+        output.push('<option label="' + title + '" value="'+ index +'">' + title +'</option>'); 
+        //output.push('</optgroup>');
+        layerList.push(layer);
       });
+      $('select').html(output.join(''));
+
+      $('select').change(function() {
+        var layer = layerList[this.selectedIndex];
+        showLayer(layer);
+      });
+
+      showLayer(layerList[0]);
     },
   });
   wps.execute();
 }
 
-function initTimeSlider(layer) {
+function showLayer(layer) {
+  if (wmsLayer != null) {
+    map.removeLayer(wmsLayer);
+  }
+  wmsLayer = new OpenLayers.Layer.WMS(layer.title,
+                                      layer.service,
+                                      {layers: layer.name,
+                                       transparent: true,
+                                       format:'image/png',
+                                       time: layer.timesteps[0]});
+  
+  map.addLayer(wmsLayer);
+  wmsLayer.events.register('loadend', wmsLayer, function(evt){
+    //map.zoomToExtent(new OpenLayers.Bounds(wmsLayer.getExtent()));
+  }); 
+  initTimeSlider(layer, wmsLayer);
+}
+
+function initTimeSlider(layer, wmsLayer) {
   var max = layer.timesteps.length - 1;
 
   // slider
@@ -73,16 +91,13 @@ function initTimeSlider(layer) {
       var step = parseInt(ui.value);
       //console.log("step: " + step);
       $("#time").text(layer.timesteps[step]);
-      layer.wms_layer.mergeNewParams({'time': layer.timesteps[step]});
+      wmsLayer.mergeNewParams({'time': layer.timesteps[step]});
     }
   });
+  $("#time").text(layer.timesteps[0]);
 
   // prev button
   $("#prev").button({
-    icons: {
-      primary: "ui-icon-gear",
-      secondary: "ui-icon-triangle-1-s"
-    },
     text: false,
   }).click(function( event ) {
     console.log('prev button clicked');
@@ -94,10 +109,6 @@ function initTimeSlider(layer) {
 
   // next button
   $("#next").button({
-    icons: {
-      primary: "ui-icon-gear",
-      secondary: "ui-icon-triangle-1-s"
-    },
     text: false,
   }).click(function( event ) {
     console.log('next button clicked');
@@ -150,8 +161,8 @@ function initRangeSlider(layer) {
     text: false,
   }).click(function( event ) {
     console.log('stop button clicked');
-    if (animate_layer != null) {
-      map.removeLayer(animate_layer);
+    if (animateLayer != null) {
+      map.removeLayer(animateLayer);
     }
   });
 }
