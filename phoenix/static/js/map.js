6f7b30ddf1ee8c91d52543f840421d759243572c
Carsten Ehbrecht <carsten@localhost>
2013-11-28 23:37:42 +0100

    refactored 2d map, disabled 3d map

--- phoenix/static/js/map.js
+++ phoenix/static/js/map.js
@@ -11,11 +11,54 @@ var opacity = 0.7;
 var layerSwitcher = null;
 
 function initMap() {
-  initGlobe();
-  initGlobeButtons();
+  //var mapOptions = { maxResolution: 256/512, numZoomLevels: 11, fractionalZoom: true};
+  //map = new OpenLayers.Map('map',mapOptions);
+  map = new OpenLayers.Map('map', { controls: [] });
+  layerSwitcher = new OpenLayers.Control.LayerSwitcher()
+  map.addControl(layerSwitcher);
+  map.addControl(new OpenLayers.Control.Navigation());
+  map.addControl(new OpenLayers.Control.PanZoom());
+  //map.addControl(new OpenLayers.Control.Attribution());
+
+  // base layer
+  var baseLayer = null;
+
+  baseLayer = new OpenLayers.Layer.WMS( 
+    "Demis BlueMarble",
+    "http://www2.demis.nl/wms/wms.ashx?WMS=BlueMarble" , 
+    {layers: 'Earth Image,Borders,Coastlines'});
+  addLayer(baseLayer);
+
+  /*
+  baseLayer = new OpenLayers.Layer.WMS( 
+    "OpenLayers WMS",
+    "http://labs.metacarta.com/wms-c/Basic.py?", {layers: 'basic'});
+  addLayer(baseLayer);
+  */
+  
+  baseLayer = new OpenLayers.Layer.WMS( 
+    "Demis WMS",
+    "http://www2.demis.nl/wms/wms.ashx?WMS=WorldMap",
+    {layers:'Countries,Bathymetry,Topography,Hillshading,Coastlines,Builtup+areas,Waterbodies,Rivers,Streams,Railroads,Highways,Roads,Trails,Borders,Cities,Airports',
+     format: 'image/png'});
+  addLayer(baseLayer);
+ 
+  map.zoomToMaxExtent();
+
+  // more controls
   initOpacitySlider();
   initLayerList();
 }
+
+function addLayer(layer) {
+  map.addLayer(layer);
+  layer.events.register('loadstart', layer, function(evt){
+    loadStarted();
+  }); 
+  layer.events.register('loadend', layer, function(evt){
+    loadFinished();
+  });
+} 
 
 function loadStarted() {
   layersLoading++;
@@ -30,185 +73,6 @@ function loadFinished() {
   }
   if (layersLoading == 0) {
     $('#loading-indicator').hide();
-  }
-}
-
-function addLayer(layer) {
-  map.addLayer(layer);
-  layer.events.register('loadstart', layer, function(evt){
-    loadStarted();
-  }); 
-  layer.events.register('loadend', layer, function(evt){
-    loadFinished();
-  }); 
-}
-
-function initBaseLayer() {
-  var baseLayer = null;
-  /*
-  baseLayer = new OpenLayers.Layer.WMS( 
-    "Demis WMS",
-    "http://www2.demis.nl/wms/wms.ashx?WMS=WorldMap",
-    {layers:'Countries,Bathymetry,Topography,Hillshading,Coastlines,Builtup+areas,Waterbodies,Rivers,Streams,Railroads,Highways,Roads,Trails,Borders,Cities,Airports',
-     format: 'image/png'});
-  addLayer(baseLayer);
-  */
-
-  /*
-  baseLayer = new OpenLayers.Layer.WMS( 
-    "OpenLayers WMS",
-    "http://labs.metacarta.com/wms-c/Basic.py?", {layers: 'basic'});
-  addLayer(baseLayer);
-  */
-
-  baseLayer = new OpenLayers.Layer.WMS( 
-    "Demis BlueMarble",
-    "http://www2.demis.nl/wms/wms.ashx?WMS=BlueMarble" , 
-    {layers: 'Earth Image,Borders,Coastlines'});
-  addLayer(baseLayer);
-}
-
-function initGlobe(show3D) {
-  show3D = show3D || false;
-
-  if (map != null) {
-    map.destroy();
-  }
-
-  //var mapOptions = { maxResolution: 256/512, numZoomLevels: 11, fractionalZoom: true};
-  //map = new OpenLayers.Map('map',mapOptions);
-  map = new OpenLayers.Map('map', { controls: [] });
-  map.setupGlobe();
-  layerSwitcher = new OpenLayers.Control.LayerSwitcher()
-  //map.addControl(layerSwitcher);
-  map.addControl(new OpenLayers.Control.Navigation());
-  map.addControl(new OpenLayers.Control.PanZoom());
-  //map.addControl(new OpenLayers.Control.Attribution());
-  initBaseLayer();
-
-  if (selectedLayer) {
-    initWMSLayer(selectedLayer, selectedTimeIndex);
-  } 
-  if (animateURL) {
-    //initAnimateLayer(animateURL);
-  }
-  map.finishGlobe();
-
-  if (show3D) {
-    map.show3D();
-  }
-  else {
-    map.show2D();
-  }
-}
-
-function initLayerList() {
-  wps = new SimpleWPS({
-    process: "org.malleefowl.wms.layer",
-    onSuccess: function(result) {
-      //console.log(result);
-      var output = [];
-      layerList = []
-      $.each(result, function(index, layer) {
-        //output.push('<optgroup label="' + layer.service_name + '">');
-        title = layer.title + " (" + layer.service_name + ")";
-        output.push('<option label="' + title + '" value="'+ index +'">' + title +'</option>'); 
-        //output.push('</optgroup>');
-        layerList.push(layer);
-      });
-      $('#select').html(output.join(''));
-
-      $('#select').change(function() {
-        selectedLayer = layerList[this.selectedIndex];
-        showWMSLayer(selectedLayer);
-      });
-
-      if (layerList.length > 0) {
-        selectedLayer = layerList[0];
-        showWMSLayer(selectedLayer);
-      }
-    },
-  });
-  wps.execute();
-}
-
-function setVisibility(animate) {
-  console.log("set visibility: " + animate);
-  if (wmsLayer != null) {
-    wmsLayer.setVisibility(true);
-    wmsLayer.displayInLayerSwitcher = false;
-  }
-  setLayerVisibility(animateLayer, animate);
-
-  layerSwitcher.layerStates = []; // forces redraw
-  layerSwitcher.redraw();
-}
-
-function setLayerVisibility(layer, visible) {
-  if (layer != null) {
-    layer.setVisibility(visible);
-    layer.displayInLayerSwitcher = visible;
-  }
-}
-
-function initWMSLayer(layer, step) {
-  console.log("init wms layer: title=" + layer.name + ", time=" + layer.timesteps[step])
-
-  wmsLayer = new OpenLayers.Layer.WMS(
-    layer.title,
-    layer.service,
-    {
-      layers: layer.name,
-      transparent: 'true',
-      format:'image/png',
-      time: layer.timesteps[step],
-    },
-    {
-      isBaseLayer: false,
-      opacity: opacity,
-    });
-  addLayer(wmsLayer);
-}
-
-function showWMSLayer(layer) {
-  if (wmsLayer != null) {
-    //console.log("remove wms layer");
-    map.removeLayer(wmsLayer);
-    wmsLayer = null;
-  }
-  initWMSLayer(layer, 0);
-  initTimeSlider(layer);
-}
-
-function dateLabel(timestep) {
-  return timestep.substring(0,16);
-}
-
-function initGlobeButtons() {
-  // 2d button
-  $("#2d").button({
-    text: true,
-  }).click(function( event ) {
-    show2D();
-  });
-
-  // 3d button
-  $("#3d").button({
-    text: true,
-  }).click(function( event ) {
-    show3D();
-  });
-}
-
-function show2D() {
-  if (map.is3D) {
-    map.show2D();
-  }
-}
-
-function show3D() {
-  if (!map.is3D) {
-    initGlobe(true);
   }
 }
 
@@ -245,6 +109,65 @@ function setOpacity(newOpacity, redraw) {
   $("#opacity-label").text("Overlay Opacity " + opacity*100 + "%");
 }
 
+function initLayerList() {
+  wps = new SimpleWPS({
+    process: "org.malleefowl.wms.layer",
+    onSuccess: function(result) {
+      //console.log(result);
+      var output = [];
+      layerList = []
+      $.each(result, function(index, layer) {
+        //output.push('<optgroup label="' + layer.service_name + '">');
+        title = layer.title + " (" + layer.service_name + ")";
+        output.push('<option label="' + title + '" value="'+ index +'">' + title +'</option>'); 
+        //output.push('</optgroup>');
+        layerList.push(layer);
+      });
+      $('#select').html(output.join(''));
+
+      $('#select').change(function() {
+        selectedLayer = layerList[this.selectedIndex];
+        showWMSLayer(selectedLayer);
+      });
+
+      if (layerList.length > 0) {
+        selectedLayer = layerList[0];
+        showWMSLayer(selectedLayer);
+      }
+    },
+  });
+  wps.execute();
+}
+
+function showWMSLayer(layer) {
+  if (wmsLayer != null) {
+    //console.log("remove wms layer");
+    map.removeLayer(wmsLayer);
+    wmsLayer = null;
+  }
+  initWMSLayer(layer, 0);
+  initTimeSlider(layer);
+}
+
+function initWMSLayer(layer, step) {
+  console.log("init wms layer: title=" + layer.name + ", time=" + layer.timesteps[step])
+
+  wmsLayer = new OpenLayers.Layer.WMS(
+    layer.title,
+    layer.service,
+    {
+      layers: layer.name,
+      transparent: 'true',
+      format:'image/png',
+      time: layer.timesteps[step],
+    },
+    {
+      isBaseLayer: false,
+      opacity: opacity,
+    });
+  addLayer(wmsLayer);
+}
+
 function initTimeSlider(layer) {
   var max = layer.timesteps.length - 1;
 
@@ -260,8 +183,6 @@ function initTimeSlider(layer) {
       $("#time").text(dateLabel(layer.timesteps[step]));
     },
     change: function(e, ui) {
-      show2D();
-      //tds_wms.setOpacity(ui.value / 10);
       var step = parseInt(ui.value);
       //console.log("step: " + step);
       $("#time").text(dateLabel(layer.timesteps[step]));
@@ -280,7 +201,6 @@ function initTimeSlider(layer) {
     //console.log('prev button clicked');
     current = $("#slider").slider( "values", 0 );
     if (current > 0 ) {
-      show2D();
       $("#slider").slider( "value", current - 1 );
     }
   });
@@ -290,7 +210,6 @@ function initTimeSlider(layer) {
     text: false,
   }).click(function( event ) {
     //console.log('next button clicked');
-    show2D();
     current = $("#slider").slider( "values", 0 );
     if (current < max ) {
       $("#slider").slider( "value", current + 1 );
@@ -312,7 +231,6 @@ function initTimeSlider(layer) {
         buttons: {
           Ok: function() {
             $( this ).dialog( "close" );
-            show2D();
             animate(selectedLayer);
           },
           Cancel: function() {
@@ -365,6 +283,29 @@ function setTimeRange(layer, min, max) {
   start_time = layer.timesteps[min];
   end_time = layer.timesteps[max];
 } 
+
+function setVisibility(animate) {
+  console.log("set visibility: " + animate);
+  if (wmsLayer != null) {
+    wmsLayer.setVisibility(true);
+    wmsLayer.displayInLayerSwitcher = false;
+  }
+  setLayerVisibility(animateLayer, animate);
+
+  layerSwitcher.layerStates = []; // forces redraw
+  layerSwitcher.redraw();
+}
+
+function setLayerVisibility(layer, visible) {
+  if (layer != null) {
+    layer.setVisibility(visible);
+    layer.displayInLayerSwitcher = visible;
+  }
+}
+
+function dateLabel(timestep) {
+  return timestep.substring(0,16);
+}
 
 function initAnimateLayer(imageURL) {
   animateLayer = new OpenLayers.Layer.Image(
