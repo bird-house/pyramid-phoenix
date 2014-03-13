ba4b1b3a5caba6ac09755c3dc5d4fb1e325c063b
Tobias Kipp <kipp@dkrz.de>
2014-03-13 15:50:58 +0100

    Added gathering of parameters as preparation for the genration of the workflow description.

--- phoenix/qc_wizard.py
+++ phoenix/qc_wizard.py
@@ -1,7 +1,10 @@
 
+import logging
+logger = logging.getLogger(__name__)
 from pyramid.view import view_config
 from pyramid.security import authenticated_userid
 from .models import user_token
+from pyramid.httpexceptions import HTTPFound
 import os
 #TODO: Put constants into a config file
 QCDIR = "/home/tk/sandbox/climdaps/var/qc_cache"
@@ -14,7 +17,7 @@ EXAMPLEDATADIR = "/home/tk/sandbox/climdaps/examples/data/CORDEX"
 def qc_wizard(request):
     title = "Quality Control Wizard"
     user_id = authenticated_userid(request)
-    token = user_token(request, user_id)
+    #token = user_token(request, user_id)
     
     parallel_id_help = ("An identifier used to avoid processes running on the same directory." + 
                         " Using an existing one will remove all data inside its work directory.")
@@ -40,24 +43,47 @@ def qc_wizard(request):
     #In that case value will be used as default if it is in allowed_values.
     #For type "checkbox" the existence of the "checked" key will lead to the checkbox being True.
     fields = [
-        {"type": "text", "text": "Parallel ID", "help":parallel_id_help,
-            "id": "parallel_id", "value": "web1"},
-        {"type": "text", "text": "Root path to the of check data",
-            "id": "data_path", "value": EXAMPLEDATADIR},
-        {"type": "select", "text": "Project", "id": "project",
+        {"id": "parallel_id", "type": "text", "text": "Parallel ID", "help":parallel_id_help,
+            "value": "web1"},
+        {"id": "data_path", "type": "text", "text": "Root path to the of check data",
+            "value": EXAMPLEDATADIR},
+        {"id": "project", "type": "select", "text": "Project", 
             "value": "CORDEX", "allowed_values": ["CORDEX"] },
-        {"type": "text", "text": "QC SELECT", "id": "select", "value": "", "help": qc_select_help},
-        {"type": "text", "text": "QC LOCK", "id": "lock", "value": "", "help": qc_lock_help},
-        {"type": "checkbox", "text": "Replica", "id": "replica", "value": ""},
-        {"type": "checkbox", "text": "Latest", "id": "latest", "value": "", "checked": "checked"},
-        {"type": "checkbox", "text": "Publish meta-data", "id": "publish_metadata", "value": "",
+        {"id": "select", "type": "text", "text": "QC SELECT", "value": "", "help": qc_select_help},
+        {"id": "lock", "type": "text", "text": "QC LOCK", "value": "", "help": qc_lock_help},
+        {"id": "replica", "type": "checkbox", "text": "Replica", "value": ""},
+        {"id": "latest", "type": "checkbox", "text": "Latest", "value": "", "checked": "checked"},
+        {"id": "publish_metadata", "type": "checkbox", "text": "Publish meta-data",  "value": "",
             "checked": "checked"},
-        {"type": "checkbox", "text": "Publish quality-data", "id": "publish_quality",
+        {"id": "publish_quality", "type": "checkbox", "text": "Publish quality-data", 
             "value": "", "checked": "checked"},
-        {"type": "checkbox", "text": "Clean afterwards", "id": "clean",
+        {"id": "clean", "type": "checkbox", "text": "Clean afterwards", 
             "value": "", "help": "Removes the work data after the steps have finished"},
         ]
     html_fields = get_html_fields(fields)
+
+    if "submit" in request.POST:
+        DATA = request.POST
+        #Create a restflow workflow (without writing it to a file).
+        #The values are gathered in the yaml workflow_description generation method
+        workflow_description = _create_qc_workflow(DATA)
+        
+        #Create a wps call for org.malleefowl.restflow
+        ###identifier = 'org.malleefowl.restflow'
+        ###inputs = [("workflow_description", str(workflow_description))]
+        ###outputs = [("output",True), ("work_output", False), ("work_status", False)]
+        ###execution = wps.execute(identifier, inputs=inputs, output=outputs)
+        ###add_job(
+        ###    request = request,
+        ###    user_id = authenticated_userid(request),
+        ###    identifier = identifier,
+        ###    wps_url = wps.url,
+        ###    execution = execution,
+        ###    notes = states[5].get('info_notes', ''),
+        ###    tags = states[5].get('info_tags', ''))
+
+
+        return HTTPFound(location=request.route_url('jobs'))
     
     return {
             "title": title,
@@ -113,3 +139,16 @@ def get_html_fields(fields):
         html_fields.append(html_field)
     return html_fields
         
+def _create_qc_workflow(DATA):
+    parallel_id = DATA["parallel_id"]
+    data_path = DATA["data_path"]
+    project =  DATA["project"]
+    select = DATA["select"]
+    lock = DATA["lock"]
+    replica = "replica" in DATA
+    latest = "latest" in DATA
+    publish_metadata = "publish_metadata" in DATA
+    publish_quality = "publish_quality" in DATA
+    clean = "clean" in DATA
+
+
