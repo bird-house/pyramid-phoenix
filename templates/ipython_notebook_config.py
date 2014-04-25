3e366ed0d8e2475dcc9cdcb228b21e6e5ccba278
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-04-25 15:10:33 +0200

    configure ipython notebook with template config, proxy ipython behind nginx

---
+++ templates/ipython_notebook_config.py
@@ -0,0 +1,16 @@
+c = get_config()
+
+# Kernel config
+c.IPKernelApp.pylab = 'inline'  # if you want plotting support always
+
+# Notebook config
+#c.NotebookApp.certfile = u'/absolute/path/to/your/certificate/mycert.pem'
+c.NotebookApp.ip = 'localhost'
+c.NotebookApp.open_browser = False
+#c.NotebookApp.password = u'sha1:bcd259ccf...[your hashed password here]'
+# It is a good idea to put it on a known, fixed port
+c.NotebookApp.port = ${ports:ipython}
+# Running with a different URL prefix
+c.NotebookApp.base_url = '/ipython/'
+c.NotebookApp.webapp_settings = {'static_url_prefix':'/ipython/static/'}
+
