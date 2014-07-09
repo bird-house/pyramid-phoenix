c1a1c36c5dbf63c7ad83cf146270314e99dd9b33
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-07-09 17:18:16 +0200

    using buildout settings options

--- templates/ipython_notebook_config.py
+++ templates/ipython_notebook_config.py
@@ -5,13 +5,12 @@ c.IPKernelApp.pylab = 'inline'  # if you want plotting support always
 
 # Notebook config
 #c.NotebookApp.certfile = u'/absolute/path/to/your/certificate/mycert.pem'
-#c.NotebookApp.ip = '${server:host}'
 c.NotebookApp.ip = '127.0.0.1'
 c.NotebookApp.open_browser = False
 #c.NotebookApp.password = u'sha1:bcd259ccf...[your hashed password here]'
-c.NotebookApp.password = ${security:ipython_password}
+c.NotebookApp.password =  u''
 # It is a good idea to put it on a known, fixed port
-c.NotebookApp.port = ${ports:ipython}
+c.NotebookApp.port = ${settings:ipython-port}
 # Running with a different URL prefix
 c.NotebookApp.base_project_url = '/ipython/notebook/'
 c.NotebookApp.base_kernel_url = '/ipython/notebook/'
