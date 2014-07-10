dab51deae00da58e5be4e5be38ed135865882edc
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-07-10 17:50:21 +0200

    update clean.sh and requirements

--- clean.sh
+++ clean.sh
@@ -9,4 +9,6 @@ rm -rf $BUILDOUT_DIR/parts
 rm -rf $BUILDOUT_DIR/bin
 rm -f $BUILDOUT_DIR/.installed.cfg
 rm -rf $BUILDOUT_DIR/*.egg-info
+rm -rf $BUILDOUT_DIR/dist
+rm -rf $BUILDOUT_DIR/build
 echo "Cleaning buildout ... Done"
--- requirements.sh
+++ requirements.sh
@@ -2,7 +2,8 @@
 if [ -f /etc/debian_version ] ; then
     sudo apt-get -y --force-yes install wget build-essential
 elif [ -f /etc/redhat-release ] ; then
-    sudo yum -y install wget
+    sudo rpm -i http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+    sudo yum -y install wget gcc-c++ 
 elif [ `uname -s` = "Darwin" ] ; then
     brew install wget
 fi
