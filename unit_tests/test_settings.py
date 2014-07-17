c79c794d88d56d188ccb72441f30369939facd2d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-07-17 18:04:34 +0200

    functional test added

--- unit_tests/test_settings.py
+++ unit_tests/test_settings.py
@@ -17,3 +17,15 @@ class UserSettingsTests(unittest.TestCase):
         response = inst.user_view()
         self.assertEqual('Home View', response['name'])
 
+class UserSettingsFunctionalTests(unittest.TestCase):
+    def setUp(self):
+        from phoenix import main
+        app = main({})
+        from webtest import TestApp
+
+        self.testapp = TestApp(app)
+
+    def test_user_view(self):
+        res = self.testapp.get('/', status=200)
+        self.assertIn(b'<h1>Hi Home View', res.body)
+
