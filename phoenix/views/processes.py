890ebac91aa58784d536a5139f189c8eed6094e8
Carsten Ehbrecht <ehbrecht@dkrz.de>
2014-08-14 14:46:51 +0200

    moved processes to views/processes.py

---
+++ phoenix/views/processes.py
@@ -0,0 +1,94 @@
+from pyramid.view import view_config, view_defaults
+
+from pyramid.httpexceptions import HTTPException, HTTPFound, HTTPNotFound
+from deform import Form, Button
+from deform import ValidationFailure
+
+from phoenix.views.views import MyView
+
+import logging
+logger = logging.getLogger(__name__)
+
+@view_defaults(permission='edit', layout='default')
+class ProcessList(MyView):
+    def __init__(self, request):
+        super(ProcessList, self).__init__(request, 'Processes')
+
+        self.wps = None
+        if 'wps.url' in self.session:
+            try:
+                from owslib.wps import WebProcessingService
+                self.wps = WebProcessingService(url=self.session['wps.url'])
+                self.description = self.wps.identification.title
+            except:
+                msg = "Could not connect to wps"
+                logger.exception(msg)
+                self.session.flash(msg, queue='error')
+
+    def sort_order(self):
+        """Determine what the current sort parameters are.
+        """
+        order = self.request.GET.get('order_col', 'identifier')
+        order_dir = self.request.GET.get('order_dir', 'asc')
+        order_dir = 1 if order_dir == 'asc' else -1
+        return dict(order=order, order_dir=order_dir)
+    
+    def generate_form(self, formid='deform'):
+        from phoenix.schema import ChooseWPSSchema
+        from phoenix.models import get_wps_list
+        schema = ChooseWPSSchema().bind(wps_list = get_wps_list(self.request))
+        return Form(
+            schema,
+            buttons=('submit',),
+            formid=formid
+            )
+    def process_form(self, form):
+        controls = self.request.POST.items()
+        try:
+            captured = form.validate(controls)
+            url = captured.get('url', '')
+            session = self.request.session
+            session['wps.url'] = url
+            session.changed()
+        except ValidationFailure, e:
+            logger.exception('validation of process view failed.')
+            return dict(form=e.render())
+        return HTTPFound(location=self.request.route_url('process_list'))
+
+    @view_config(route_name='process_list', renderer='phoenix:templates/process_list.pt')
+    def view(self):
+        form = self.generate_form()
+        if 'submit' in self.request.POST:
+            return self.process_form(form)
+
+        items = []
+        if self.wps is not None:
+            for process in self.wps.processes:
+                # TODO: sometime no abstract avail. Fix handling this in owslib
+                abstract = ''
+                if hasattr(process, 'abstract'):
+                    abstract = process.abstract
+                processVersion = ''
+                if hasattr(process, 'processVersion'):
+                    processVersion = process.processVersion
+                items.append(dict(title=process.title,
+                                  identifier=process.identifier,
+                                  abstract=abstract,
+                                  version=processVersion))
+
+        # sort items
+        order = self.sort_order()
+        import operator
+        items.sort(key=operator.itemgetter(order['order']), reverse=order['order_dir']==-1)
+
+        from phoenix.grid import ProcessesGrid
+        grid = ProcessesGrid(
+                self.request,
+                items,
+                ['title', 'abstract', 'action'],
+            )
+        return dict(
+            grid=grid,
+            items=items,
+            form=form.render())
+
