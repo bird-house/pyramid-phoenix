81e1aee07d0ee27a777a275508a9f474ea95882a
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-05-04 16:57:58 +0200

    simplified job title in myjobs view

--- phoenix/views/myjobs/overview.py
+++ phoenix/views/myjobs/overview.py
@@ -68,7 +68,7 @@ class Overview(MyJobs):
         grid = JobsGrid(
                 self.request,
                 items,
-                ['status', 'title', 'duration', 'finished', 'progress', ''],
+                ['status', 'job', 'duration', 'finished', 'progress', ''],
             )
         return dict(grid=grid, items=items)
 
@@ -79,15 +79,18 @@ class JobsGrid(MyGrid):
     def __init__(self, request, *args, **kwargs):
         super(JobsGrid, self).__init__(request, *args, **kwargs)
         self.column_formats['status'] = self.status_td
+        self.column_formats['job'] = self.job_td
         self.column_formats['duration'] = self.duration_td
         self.column_formats['finished'] = self.finished_td
-        self.column_formats['title'] = self.title_td
         self.column_formats['progress'] = self.progress_td
         self.column_formats[''] = self.action_td
         self.exclude_ordering = self.columns
 
     def status_td(self, col_num, i, item):
         return self.render_status_td(item)
+    
+    def job_td(self, col_num, i, item):
+        return self.render_label_td(item['title'])
 
     def duration_td(self, col_num, i, item):
         try:
@@ -107,9 +110,6 @@ class JobsGrid(MyGrid):
             time_ago = '???'
         finally:
             return self.render_label_td(time_ago)
-
-    def title_td(self, col_num, i, item):
-        return self.render_title_td(item['title'], '', item['keywords'].split(','))
 
     def progress_td(self, col_num, i, item):
         return self.render_progress_td(identifier=item.get('identifier'), progress = item.get('progress', 0))
