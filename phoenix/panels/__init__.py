30d0493455477496a702ee4386f2c75f189aa354
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-05-11 18:38:23 +0200

    reorder navbar

--- phoenix/panels/__init__.py
+++ phoenix/panels/__init__.py
@@ -18,16 +18,17 @@ def navbar(context, request):
 
     items = []
     if has_permission('edit', request.context, request):
-        items.append( nav_item('Dashboard', request.route_path('dashboard', tab='jobs')) )
         items.append( nav_item('Processes', request.route_path('processes_overview')) )
     if has_permission('submit', request.context, request):
         items.append( nav_item('My Jobs', request.route_path('myjobs_overview')) )
         items.append( nav_item('Wizard', request.route_path('wizard')) )
     if has_permission('edit', request.context, request):
         items.append( nav_item('My Account', request.route_path('myaccount', tab='profile')) )
+    items.append( nav_item('Help', request.route_url('readthedocs')) )
+    if has_permission('edit', request.context, request):
+        items.append( nav_item('Dashboard', request.route_path('dashboard', tab='jobs')) )
     if has_permission('admin', request.context, request):
         items.append( nav_item('Settings', request.route_path('settings')) )
-    items.append( nav_item('Help', request.route_url('readthedocs')) )
 
     login = request.current_route_url() == request.route_url('account_login', protocol='esgf')
 
