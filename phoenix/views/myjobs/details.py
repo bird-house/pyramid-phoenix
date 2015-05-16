9870a6f4c45e15ff269c7e35371db55e62b24274
Carsten Ehbrecht <carsten@linacs.org>
2015-05-16 19:50:10 +0200

    disabled worklow outputs in job details

--- phoenix/views/myjobs/details.py
+++ phoenix/views/myjobs/details.py
@@ -69,12 +69,12 @@ class JobDetails(MyJobs):
         lm = self.request.layout_manager
         if tab == 'log':
             lm.layout.add_heading('myjobs_log')
-        elif tab == 'inputs':
-            lm.layout.add_heading('myjobs_inputs')
-        elif tab == 'resources':
-            lm.layout.add_heading('myjobs_resources')
-        elif tab == 'workflow':
-            lm.layout.add_heading('myjobs_workflow')
+        #elif tab == 'inputs':
+        #    lm.layout.add_heading('myjobs_inputs')
+        #elif tab == 'resources':
+        #    lm.layout.add_heading('myjobs_resources')
+        #elif tab == 'workflow':
+        #    lm.layout.add_heading('myjobs_workflow')
         else:
             lm.layout.add_heading('myjobs_outputs')
 
