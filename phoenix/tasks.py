73e1b77f2ab4f539b58142d08b68e6f19b15d3cb
Carsten Ehbrecht <carsten@linacs.org>
2015-05-16 22:34:52 +0200

    working on workflow execution

--- phoenix/tasks.py
+++ phoenix/tasks.py
@@ -1,4 +1,10 @@
 from pyramid_celery import celery_app as app
+
+from owslib.wps import WebProcessingService, monitorExecution
+import json
+import uuid
+from datetime import datetime
+
 from phoenix.models import mongodb
 
 def task_result(task_id):
@@ -12,7 +18,6 @@ def esgf_logon(email, url, openid, password):
     inputs.append( ('password', password.encode('ascii', 'ignore')) )
     outputs = [('output',True),('expires',False)]
 
-    from owslib.wps import WebProcessingService, monitorExecution
     wps = WebProcessingService(url=url, skip_caps=True)
     execution = wps.execute(identifier="esgf_logon", inputs=inputs, output=outputs)
     monitorExecution(execution)
@@ -27,39 +32,33 @@ def esgf_logon(email, url, openid, password):
         db.users.update({'email':email}, user)
     return execution.status
 
-
 @app.task
 def execute_workflow(email, url, name, nodes):
-    import json
+    registry = app.conf['PYRAMID_REGISTRY']
+
     nodes_json = json.dumps(nodes)
     # generate and run dispel workflow
     identifier='dispel'
     inputs=[('nodes', nodes_json), ('name', name)]
     outputs=[('output', True)]
-    return execute_process(email, url, identifier, 
-                           inputs=inputs, outputs=outputs, workflow=True)
-
-@app.task
-def execute_process(email, url, identifier, inputs, outputs, workflow=False, keywords=None):
-    registry = app.conf['PYRAMID_REGISTRY']
-    from owslib.wps import WebProcessingService
+    
     wps = WebProcessingService(url=url, skip_caps=True)
     execution = wps.execute(identifier, inputs=inputs, output=outputs)
     db = mongodb(registry)
-    import uuid
-    from datetime import datetime
+    
     job = dict(
         identifier = uuid.uuid4().get_hex(),
-        workflow = workflow,
-        title = execution.process.title,
-        abstract = execution.process.abstract,
-        keywords = keywords,
+        workflow = True,
+        title = nodes['worker']['identifier'],
+        abstract = '',
+        keywords = None,
         email = email,
         wps_url = execution.serviceInstance,
         status_location = execution.statusLocation,
         created = datetime.now(),
         is_complete = False)
     db.jobs.save(job)
+
     while not execution.isComplete():
         execution.checkStatus(sleepSecs=2)
         job['status'] = execution.getStatus()
@@ -75,6 +74,44 @@ def execute_process(email, url, identifier, inputs, outputs, workflow=False, key
             job['progress'] = 100
         else:
             job['progress'] = execution.percentCompleted
-        # update db
+        db.jobs.update({'identifier': job['identifier']}, job)
+    return execution.getStatus()
+
+@app.task
+def execute_process(email, url, identifier, inputs, outputs, workflow=False, keywords=None):
+    registry = app.conf['PYRAMID_REGISTRY']
+
+    wps = WebProcessingService(url=url, skip_caps=True)
+    execution = wps.execute(identifier, inputs=inputs, output=outputs)
+    db = mongodb(registry)
+
+    job = dict(
+        identifier = uuid.uuid4().get_hex(),
+        workflow = workflow,
+        title = execution.process.title,
+        abstract = execution.process.abstract,
+        keywords = keywords,
+        email = email,
+        wps_url = execution.serviceInstance,
+        status_location = execution.statusLocation,
+        created = datetime.now(),
+        is_complete = False)
+    db.jobs.save(job)
+
+    while not execution.isComplete():
+        execution.checkStatus(sleepSecs=2)
+        job['status'] = execution.getStatus()
+        job['status_message'] = execution.statusMessage
+        job['is_complete'] = execution.isComplete()
+        job['is_succeded'] = execution.isSucceded()
+        job['errors'] = [ '%s %s\n: %s' % (error.code, error.locator, error.text.replace('\\','')) for error in execution.errors]
+        duration = datetime.now() - job.get('created', datetime.now())
+        job['duration'] = str(duration).split('.')[0]
+        if execution.isComplete():
+            job['finished'] = datetime.now()
+        if execution.isSucceded():
+            job['progress'] = 100
+        else:
+            job['progress'] = execution.percentCompleted
         db.jobs.update({'identifier': job['identifier']}, job)
     return execution.getStatus()
