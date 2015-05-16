8b59d43c36d64ef5c6fa5eee3e9a1c51edfeaeb7
Carsten Ehbrecht <carsten@linacs.org>
2015-05-16 18:49:32 +0200

    cleaned up esgf loading page ...

---
+++ phoenix/static/scripts/esgflogon.js
@@ -0,0 +1,23 @@
+$(function() {
+
+  var timerId = 0;
+
+  var checkLogon = function() {
+    $.getJSON(
+      '/wizard/check_logon.json',
+      {},
+      function(json) {
+        if (json.status == 'ready') {
+          clearInterval(timerId);
+          location.reload();
+        }
+      }
+    );
+  };
+
+  // refresh job list each 1 secs ...
+  timerId = setInterval(function() {
+    checkLogon();
+  }, 1000);
+ 
+});
