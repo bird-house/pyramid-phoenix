74bd771bbf81457ee93d77b539966cf1e6e0484e
Carsten Ehbrecht <carsten@linacs.org>
2015-05-17 23:42:37 +0200

    changed myjobs order in navbar

--- phoenix/panels/__init__.py
+++ phoenix/panels/__init__.py
@@ -15,9 +15,9 @@ def navbar(context, request):
     if has_permission('edit', request.context, request):
         items.append( nav_item('Processes', request.route_path('processes')) )
     if has_permission('submit', request.context, request):
-        items.append( nav_item('My Jobs', request.route_path('myjobs')) )
         items.append( nav_item('Wizard', request.route_path('wizard')) )
-    
+        items.append( nav_item('My Jobs', request.route_path('myjobs')) )
+        
     subitems = []
     if has_permission('edit', request.context, request):
         subitems.append( nav_item('Profile', request.route_path('profile', tab='account'), icon="glyphicon-user") )
