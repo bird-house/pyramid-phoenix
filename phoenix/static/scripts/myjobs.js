b81c060efb358098b8077100c2e5925f0bfb8be0
Carsten Ehbrecht <carsten@linacs.org>
2015-05-17 15:59:11 +0200

    fixed job list refresh

--- phoenix/static/scripts/myjobs.js
+++ phoenix/static/scripts/myjobs.js
@@ -7,13 +7,18 @@ $(function() {
 
   // update job table with current values
   var updateJobs = function() {
-     $.getJSON(
+    $.getJSON(
       '/myjobs/update.json',
       {},
       function(json) {
         //var finished = true;
 	var finished = false;
-        $.each(json, function(index, job) {
+	var numRows = $("table tr").length - 1;
+	if (numRows != json.length) {
+	  location.reload();
+	}
+	else {
+          $.each(json, function(index, job) {
             var status_class = ''
             if (job.status == 'ProcessSucceeded') {
               status_class = 'glyphicon glyphicon-ok-sign text-success';
@@ -31,14 +36,15 @@ $(function() {
             else {
               status_class = 'glyphicon glyphicon-question-sign text-danger';
             }
-
+	    
             $("#status-"+job.identifier).attr('class', status_class);
             $("#status-"+job.identifier).attr('title', job.status);
             $("#duration-"+job.identifier).text(job.duration);
             $("#progress-"+job.identifier).attr('style', "width: "+job.progress+"%");
             $("#progress-"+job.identifier).text(job.progress + "%");
-        });
-
+          });
+	}
+	
         if (finished == true) {
           clearInterval(timerId);
         }
