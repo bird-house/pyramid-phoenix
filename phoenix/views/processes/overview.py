dfd3b188376310cf7d2a8532c95e7a14ea5b02de
Carsten Ehbrecht <carsten@linacs.org>
2015-05-23 18:12:22 +0200

    using media list for wps list

--- phoenix/views/processes/overview.py
+++ phoenix/views/processes/overview.py
@@ -1,7 +1,6 @@
 from pyramid.view import view_config, view_defaults
 
 from phoenix.views.processes import Processes
-from phoenix.grid import MyGrid
 from phoenix import models
 
 import logging
@@ -17,26 +16,11 @@ class Overview(Processes):
 
     @view_config(route_name='processes', renderer='phoenix:templates/processes/overview.pt')
     def view(self):
-        items = models.get_wps_list(self.request)
-
-        grid = OverviewGrid(
-                self.request,
-                items,
-                ['Web Processing Service'],
-            )
-        return dict(grid=grid)
-
-class OverviewGrid(MyGrid):
-    def __init__(self, request, *args, **kwargs):
-        super(OverviewGrid, self).__init__(request, *args, **kwargs)
-        self.labels['Web Processing Service'] = ''
-        self.column_formats['Web Processing Service'] = self.title_td
-        self.exclude_ordering = self.columns
-
-    def title_td(self, col_num, i, item):
-        return self.render_title_td(
-            title=item.get('title'), 
-            abstract=item.get('abstract'),
-            url=self.request.route_path('processes_list', _query=[('url', item.get('source'))]))
+        wps_list = models.get_wps_list(self.request)
+        items = []
+        for wps in wps_list:
+            url=self.request.route_path('processes_list', _query=[('url', wps.get('source'))])
+            items.append(dict(title=wps.get('title'), description=wps.get('abstract'), url=url))
+        return dict(title="Web Processing Services", items=items)
 
     
