243bda5e7f7c421f5c20c0281428dbba4c1acd35
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-06-17 16:56:38 +0200

    added ldap packages

--- requirements.sh
+++ requirements.sh
@@ -3,6 +3,12 @@
 if [ -f /etc/debian_version ] ; then
     echo "Installing Debian/Ubuntu packages ..."
     #sudo apt-get -y install wget
+    # FK: The following dependencies currently only apply to the ldap branch of
+    #     pyramid-phoenix (which is 'krueckedev')
+    sudo apt-get -y install libldap2-dev libsasl2-dev python-all-dev # needed by pyramid_ldap
+    # FK: The cleaner way would be to invoke 'sudo apt-get -y build-dep python-ldap'
+    #     but source code repo's are likely to be disabled so that this one would fail.
+    # TODO: Add these dependencies for the other platforms as well.
 elif [ -f /etc/redhat-release ] ; then
     echo "Installing RedHat/CentOS packages ..."
     #sudo yum -y install wget
