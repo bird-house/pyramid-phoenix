544ae9d90b74b6dc0ce884c2a67fb4a277bc7f4e
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-07-27 17:38:01 +0200

    added solr ulr for workflow

--- phoenix/wizard/views/done.py
+++ phoenix/wizard/views/done.py
@@ -57,6 +57,7 @@ class Done(Wizard):
         elif 'solr' in source_type:
             state = self.wizard_state.get('wizard_solr')
             source = dict()
+            source['url'] = 'http://localhost:8983/solr/birdhouse/'
             solr_query = state.get('query', '')
             if len(solr_query.strip()) == 0:
                 solr_query = '*:*'
