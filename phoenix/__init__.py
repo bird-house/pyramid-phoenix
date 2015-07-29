8908346662e378252ed01a1226d8c28558c8880c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-07-29 11:28:07 +0200

    working on solr settings page

--- phoenix/__init__.py
+++ phoenix/__init__.py
@@ -87,6 +87,7 @@ def main(global_config, **settings):
     config.add_route('settings_monitor', '/settings/monitor')
     config.add_route('settings_auth', '/settings/auth')
     config.add_route('settings_ldap', '/settings/ldap')
+    config.add_route('settings_solr', '/settings/solr')
 
     # supervisor
     config.add_route('supervisor', '/supervisor')
---
+++ phoenix/settings/views/solr.py
@@ -0,0 +1,23 @@
+from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPFound
+
+from . import SettingsView
+
+import logging
+logger = logging.getLogger(__name__)
+
+class SolrSettings(SettingsView):
+    def __init__(self, request):
+        super(SolrSettings, self).__init__(request, name='settings_solr', title='Solr')
+        self.tasksdb = self.request.db.tasks
+
+    def breadcrumbs(self):
+        breadcrumbs = super(SolrSettings, self).breadcrumbs()
+        breadcrumbs.append(dict(route_path=self.request.route_path(self.name), title=self.title))
+        return breadcrumbs
+
+    @view_config(route_name='settings_solr', renderer='../templates/settings/solr.pt')
+    def view(self):
+        return dict()
+
+
