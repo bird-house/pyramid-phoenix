48c788d7afe5b654dbadc26ad45b3785e1ce7af3
Carsten Ehbrecht <ehbrecht@dkrz.de>
2015-07-30 15:25:12 +0200

    using solr recipe 0.1.3

--- phoenix/wizard/views/solrsearch.py
+++ phoenix/wizard/views/solrsearch.py
@@ -65,21 +65,20 @@ class SolrSearch(Wizard):
             url = self.request.registry.settings.get('solr.url')
             solr = pysolr.Solr(url, timeout=10)
             options = {'start':start, 'rows':rows,
-                       'facet':'true', 'facet.field':['category', 'source', 'text'],
+                       'facet':'true', 'facet.field':['category', 'source', 'tags'],
                        'facet.limit': 300, 'facet.mincount': 1}
             if tag or category or source:
                 options['fq'] = []
                 if category:
                     options['fq'].append('category:{0}'.format(category))
                 if source:
-                    options['fq'].append( 'source:{0}'.format(source))
+                    options['fq'].append('source:{0}'.format(source))
                 if tag:
-                    options['fq'].append( 'text:{0}'.format(tag))
+                    options['fq'].append('tags:{0}'.format(tag))
             results = solr.search(solr_query, **options)
             sources = results.facets['facet_fields']['source'][::2]
-            tag_values = results.facets['facet_fields']['text'][::2]
-            tag_counts = results.facets['facet_fields']['text'][1::2]
-            logger.debug(results.facets['facet_fields']['text'])
+            tag_values = results.facets['facet_fields']['tags'][::2]
+            tag_counts = results.facets['facet_fields']['tags'][1::2]
             tags = []
             for i in xrange(0, len(tag_counts)):
                 if tag_counts[i] == results.hits:
