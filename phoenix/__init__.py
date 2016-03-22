f8d4011e601cd6d865e5a261770dc04ddd7aedcb
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-03-22 16:09:39 +0100

    using includeme for settings

--- phoenix/__init__.py
+++ phoenix/__init__.py
@@ -79,14 +79,7 @@ def main(global_config, **settings):
     config.add_route('forget_esgf_certs', '/forget_esgf_certs')
 
     # settings
-    config.add_route('settings', '/settings')
-    config.add_route('settings_users', '/settings/users')
-    config.add_route('settings_edit_user', '/settings/users/{userid}/edit')
-    config.add_route('remove_user', '/settings/users/{userid}/remove')
-    config.add_route('settings_monitor', '/settings/monitor')
-    config.add_route('settings_auth', '/settings/auth')
-    config.add_route('settings_ldap', '/settings/ldap')
-    config.add_route('settings_solr', '/settings/solr/{tab}')
+    config.include('phoenix.settings')
 
     # supervisor
     config.add_route('supervisor', '/supervisor')
---
+++ phoenix/settings/__init__.py
@@ -0,0 +1,16 @@
+import logging
+logger = logging.getLogger(__name__)
+
+def includeme(config):
+    settings = config.registry.settings
+
+    logger.info('Add Settings')
+
+    config.add_route('settings', '/settings')
+    config.add_route('settings_users', '/settings/users')
+    config.add_route('settings_edit_user', '/settings/users/{userid}/edit')
+    config.add_route('remove_user', '/settings/users/{userid}/remove')
+    config.add_route('settings_monitor', '/settings/monitor')
+    config.add_route('settings_auth', '/settings/auth')
+    config.add_route('settings_ldap', '/settings/ldap')
+    config.add_route('settings_solr', '/settings/solr/{tab}')
