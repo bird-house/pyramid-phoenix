e4f90a8e086a404122e36433ebee7041616c9d95
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-05-30 15:37:31 +0200

    using job_to_state in wizard

--- phoenix/wizard/views/start.py
+++ phoenix/wizard/views/start.py
@@ -11,6 +11,46 @@ from phoenix.wizard.views import Wizard
 import logging
 logger = logging.getLogger(__name__)
 
+def job_to_state(request):
+    # TODO: quite dirty ... needs clean up
+    state = {}
+    job = request.db.jobs.find_one({'identifier': request.params['job_id']})
+    execution = WPSExecution()
+    execution.checkStatus(url=job['status_location'], sleepSecs=0)
+    if len(execution.dataInputs) == 1:
+        if len(execution.dataInputs[0].data) == 1:
+            workflow = yaml.load(execution.dataInputs[0].data[0])
+           
+            state['wizard_wps'] = {'identifier': wps_id(request, workflow['worker']['url'])}
+            state['wizard_process'] = {'identifier': workflow['worker']['identifier']}
+            inputs = {}
+            for inp in workflow['worker']['inputs']:
+                key, value = inp[0], inp[1]
+                if key in inputs:
+                    inputs[key].extend(value)
+                else:
+                    inputs[key] = [value]
+            wps = WebProcessingService(url=workflow['worker']['url'], verify=False)
+            process = wps.describeprocess(workflow['worker']['identifier'])
+            for inp in process.dataInputs:
+                if 'boolean' in inp.dataType and inp.identifier in result:
+                    inputs[inp.identifier] = [ val.lower() == 'true' for val in inputs[inp.identifier]]
+                if inp.maxOccurs < 2 and inp.identifier in inputs:
+                    inputs[inp.identifier] = inputs[inp.identifier][0]
+            state['wizard_literal_inputs'] = inputs
+            state['wizard_complex_inputs'] = {'identifier': workflow['worker']['resource']}
+            if workflow['name'] == 'wizard_esgf_search':
+                state['wizard_source'] = {'source': 'wizard_esgf_search'}
+                import json
+                state['wizard_esgf_search'] = {'selection': json.dumps(workflow['source']['esgf'])}
+            elif workflow['name'] == 'wizard_solr':
+                state['wizard_source'] = {'source': 'wizard_solr'}
+            elif workflow['name'] == 'wizard_threddsservice':
+                state['wizard_source'] = {'source': 'wizard_threddsservice'}
+                state['wizard_threddsservice'] = {'url': workflow['source']['thredds']['catalog_url']}
+    return state
+
+
 import colander
 from deform.widget import SelectWidget
 class Schema(colander.MappingSchema):
@@ -30,53 +70,16 @@ class Start(Wizard):
         self.wizard_state.clear()
         self.favorite.load()
 
-        self.workflow = None
         if 'job_id' in request.params:
-            job = request.db.jobs.find_one({'identifier': request.params['job_id']})
-            execution = WPSExecution()
-            execution.checkStatus(url=job['status_location'], sleepSecs=0)
-            if len(execution.dataInputs) == 1:
-                if len(execution.dataInputs[0].data) == 1:
-                    self.workflow = yaml.load(execution.dataInputs[0].data[0])
-                    logger.debug('workflow = %s', self.workflow)
-                    state = {}
-                    state['wizard_wps'] = {'identifier': wps_id(request, self.workflow['worker']['url'])}
-                    state['wizard_process'] = {'identifier': self.workflow['worker']['identifier']}
-                    inputs = {}
-                    for inp in self.workflow['worker']['inputs']:
-                        key, value = inp[0], inp[1]
-                        if key in inputs:
-                            inputs[key].extend(value)
-                        else:
-                            inputs[key] = [value]
-                    wps = WebProcessingService(url=self.workflow['worker']['url'], verify=False)
-                    process = wps.describeprocess(self.workflow['worker']['identifier'])
-                    for inp in process.dataInputs:
-                        if 'boolean' in inp.dataType and inp.identifier in result:
-                            inputs[inp.identifier] = [ val.lower() == 'true' for val in inputs[inp.identifier]]
-                        if inp.maxOccurs < 2 and inp.identifier in inputs:
-                            inputs[inp.identifier] = inputs[inp.identifier][0]
-                    state['wizard_literal_inputs'] = inputs
-                    state['wizard_complex_inputs'] = {'identifier': self.workflow['worker']['resource']}
-                    if self.workflow['name'] == 'wizard_esgf_search':
-                        state['wizard_source'] = {'source': 'wizard_esgf_search'}
-                        import json
-                        state['wizard_esgf_search'] = {'selection': json.dumps(self.workflow['source']['esgf'])}
-                    elif self.workflow['name'] == 'wizard_solr':
-                        state['wizard_source'] = {'source': 'wizard_solr'}
-                    elif self.workflow['name'] == 'wizard_threddsservice':
-                        state['wizard_source'] = {'source': 'wizard_threddsservice'}
-                        state['wizard_threddsservice'] = {'url': self.workflow['source']['thredds']['catalog_url']}
-                    self.favorite.set(name=self.workflow['worker']['identifier'], state=state)
+            state = job_to_state(request)
+            self.favorite.set(name='restart', state=state)
             
     def schema(self):
         favorites = self.favorite.names()
         return Schema().bind(favorites=favorites)
 
     def appstruct(self):
-        result = {}
-        if self.workflow:
-            result = {'favorite': self.workflow['worker']['identifier']}
+        result = {'favorite': 'restart'}
         return result
 
     def success(self, appstruct):
