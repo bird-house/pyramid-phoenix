fe5be94081815717a2518526f502ef6524774302
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-05-31 17:33:23 +0200

    removed old catalog code

--- phoenix/catalog.py
+++ phoenix/catalog.py
@@ -134,40 +134,6 @@ class MongodbCatalog(Catalog):
     def __init__(self, collection):
         self.collection = collection
 
-def wps_id(request, name):
-    # TODO: fix retrieval of wps id
-    #wps_query = PropertyIsEqualTo('dc:format', 'WPS')
-    title_query = PropertyIsEqualTo('dc:title', name)
-    request.csw.getrecords2(esn="full", constraints=[title_query], maxrecords=1)
-    logger.debug("csw results %s", request.csw.results)
-    identifier = None
-    if len(request.csw.records.values()) == 1:
-        rec = request.csw.records.values()[0]
-        identifier = rec.identifier
-        logger.debug("found rec %s %s %s", rec.identifier, rec.title, rec.source)
-    return identifier
-
-def wps_url(request, identifier):
-    request.csw.getrecordbyid(id=[identifier])
-    record = request.csw.records[identifier]
-    registry = service_registry_factory(request.registry)
-    # TODO: fix service name
-    service_name = record.title.lower()
-    registry.register_service(name=service_name, url=record.source)
-    url = proxy_url(request, service_name)
-    logger.debug("identifier=%s, source=%s, url=%s", identifier, record.source, url)
-    return url
-
-def get_wps_list(request):
-    wps_query = PropertyIsEqualTo('dc:format', 'WPS')
-    request.csw.getrecords2(esn="full", constraints=[wps_query], maxrecords=100)
-    return request.csw.records.values()
-
-def get_thredds_list(request):
-    wps_query = PropertyIsEqualTo('dc:format', 'THREDDS')
-    request.csw.getrecords2(esn="full", constraints=[wps_query], maxrecords=100)
-    return request.csw.records.values()
-
 def publish(request, record):
     record['identifier'] = uuid.uuid4().get_urn()
     templ_dc = Template(filename=join(dirname(__file__), "templates", "catalog", "dublin_core.xml"))
