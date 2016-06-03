6b909824bb0c4aecbbf1ef7022bf4822a884e5e4
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-06-03 13:25:52 +0200

    test doc2record none values

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -5,3 +5,5 @@ from phoenix.catalog import doc2record
 def test_doc2record():
     record = doc2record({'_id': '123', 'title': 'test doc'})
     assert record.title == 'test doc'
+
+    assert doc2record(None) == None
