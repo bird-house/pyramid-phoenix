fb03207ce37b6d8ab27d57e8507ba7f6d7105d7d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-07-06 16:29:08 +0200

    working on monitor.js

--- phoenix/static/scripts/monitor.js
+++ phoenix/static/scripts/monitor.js
@@ -8,44 +8,11 @@ $(function() {
   // update job table with current values
   var updateJobs = function() {
     $.getJSON(
-      '/monitor/update.json',
+      '/active_jobs.json',
       {},
       function(json) {
-        //var finished = true;
-        var finished = false;
-        var numRows = $("table tr").length - 1;
-        if (numRows < json.length) {
-          location.reload();
-        }
-        else {
-          $.each(json, function(index, job) {
-            var status_class = ''
-            if (job.status == 'ProcessSucceeded') {
-              status_class = 'glyphicon glyphicon-ok-sign text-success';
-            }
-            else if (job.status == 'ProcessFailed') {
-              status_class = 'glyphicon glyphicon-remove-sign text-danger';
-            }
-            else if (job.status == 'ProcessPaused') {
-              status_class = 'glyphicon glyphicon-paused text-muted';
-            }
-            else if (job.status == 'ProcessStarted' || job.status == 'ProcessAccepted') {
-              status_class = 'fa fa-cog fa-spin text-muted';
-              //finished = false;
-            }
-            else {
-              status_class = 'glyphicon glyphicon-question-sign text-danger';
-            }
-    
-            $("#status-"+job.identifier).attr('class', status_class);
-            $("#status-"+job.identifier).attr('title', job.status);
-            $("#duration-"+job.identifier).text(job.duration);
-            $("#progress-"+job.identifier).attr('style', "width: "+job.progress+"%");
-            $("#progress-"+job.identifier).text(job.progress + "%");
-          });
-        }
-        
-        if (finished == true) {
+        location.reload();
+        if (json.length == 0) {
           clearInterval(timerId);
         }
       }
@@ -54,8 +21,7 @@ $(function() {
 
   // refresh job list each 5 secs ...
   timerId = setInterval(function() {
-    //updateJobs();
-    location.reload();
+    updateJobs();
   }, 5000); 
   
 
