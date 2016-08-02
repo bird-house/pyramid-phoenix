71459de1b469a74375f0474b1fe80088c89838e2
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-02 14:20:16 +0200

    added layer control

--- phoenix/map/templates/map/map.js
+++ phoenix/map/templates/map/map.js
@@ -5,46 +5,54 @@ var map = L.map('map', {
   timeDimension: true,
   timeDimensionOptions:{
         times: "${times}",
-    //timeInterval: "2001-01-16T12:00:00Z/2005-12-16T12:00:00Z",
-    //period: "P1M"
   },
   timeDimensionControl: true,
-  timeDimensionControlOptions:{
-    //timeSteps: 12
-  },
 % endif    
   center: [20.0, 0.0],
 });
 
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'    
-}).addTo(map);
+});
+osmLayer.addTo(map);
 
 % if dataset:
 var testWMS = "/ows/proxy/wms?DATASET=${dataset}"
-var testLayer = L.tileLayer.wms(testWMS, {
+var ncLayer = L.tileLayer.wms(testWMS, {
   layers: '${layers}',
   format: 'image/png',
   transparent: true,
   styles: '${styles}',
   attribution: '<a href="http://bird-house.github.io/">Birdhouse</a>'
 });
-var testTimeLayer = L.timeDimension.layer.wms(testLayer, {
+var ncTimeLayer = L.timeDimension.layer.wms(ncLayer, {
   updateTimeDimension: false,
 });
-testTimeLayer.addTo(map);
+ncTimeLayer.addTo(map);
+% endif
 
-var testLegend = L.control({
+var baseMaps = {
+  "OpenStreetMap": osmLayer,
+};
+
+var overlayMaps = {
+  "${layers}": ncTimeLayer
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+% if dataset:
+var ncLegend = L.control({
     position: 'topright'
 });
-testLegend.onAdd = function(map) {
+ncLegend.onAdd = function(map) {
     var src = testWMS + "&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetLegendGraphic&LAYERS=${layers}&STYLES=${styles}&PALETTE=default&HEIGHT=300";
     var div = L.DomUtil.create('div', 'info legend');
     div.innerHTML +=
         '<img src="' + src + '" alt="legend">';
     return div;
 };
-testLegend.addTo(map);
+ncLegend.addTo(map);
 % endif
 
 L.control.coordinates({
