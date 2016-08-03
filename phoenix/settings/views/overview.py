2399b185b7580b7c7f947126dc65eb6d8ad1a4de
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-03 17:33:27 +0200

    moved fatcow icons ... add logo and favicon

--- phoenix/settings/views/overview.py
+++ phoenix/settings/views/overview.py
@@ -14,14 +14,14 @@ class Overview(SettingsView):
         buttongroups = []
         buttons = []
 
-        buttons.append(dict(url=self.request.route_path('supervisor'), icon="monitor_edit.png", title="Supervisor"))
-        buttons.append(dict(url=self.request.route_path('services'), icon="bookshelf.png", title="Services"))
+        buttons.append(dict(url=self.request.route_path('supervisor'), icon="fatcow/img/monitor_edit.png", title="Supervisor"))
+        buttons.append(dict(url=self.request.route_path('services'), icon="fatcow/img/bookshelf.png", title="Services"))
         if self.request.solr_activated:
-            buttons.append(dict(url=self.request.route_path('settings_solr', tab='index'), icon="solr.png", title="Solr"))
-        buttons.append(dict(url=self.request.route_path('settings_users'), icon="user_catwomen.png", title="Users"))
-        buttons.append(dict(url=self.request.route_path('settings_auth'), icon="lock_edit.png", title="Auth"))
-        buttons.append(dict(url=self.request.route_path('settings_ldap'), icon="ldap.png", title="LDAP"))
-        buttons.append(dict(url=self.request.route_path('settings_github'), icon="Octocat.jpg", title="GitHub"))
+            buttons.append(dict(url=self.request.route_path('settings_solr', tab='index'), icon="phoenix/img/solr.png", title="Solr"))
+        buttons.append(dict(url=self.request.route_path('settings_users'), icon="fatcow/img/user_catwomen.png", title="Users"))
+        buttons.append(dict(url=self.request.route_path('settings_auth'), icon="fatcow/img/lock_edit.png", title="Auth"))
+        buttons.append(dict(url=self.request.route_path('settings_ldap'), icon="phoenix/img/ldap.png", title="LDAP"))
+        buttons.append(dict(url=self.request.route_path('settings_github'), icon="phoenix/img/Octocat.jpg", title="GitHub"))
         buttongroups.append(dict(title='Settings', buttons=buttons))
 
         return dict(buttongroups=buttongroups)
