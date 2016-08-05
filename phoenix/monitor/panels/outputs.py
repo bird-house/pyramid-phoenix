e421447c35b1376d4eade9e59818dc3ffa9ce1a3
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-05 13:42:59 +0200

    removed publish form (currently unused)

--- phoenix/monitor/panels/outputs.py
+++ phoenix/monitor/panels/outputs.py
@@ -1,10 +1,5 @@
 from pyramid_layout.panel import panel_config
 
-from pyramid.httpexceptions import HTTPFound
-from deform import Form
-from deform import ValidationFailure
-
-from phoenix import swift
 from phoenix.grid import CustomGrid
 
 import logging
@@ -37,67 +32,10 @@ class Outputs(object):
         self.context = context
         self.request = request
         self.session = self.request.session
-    
-    def generate_publish_form(self, formid="deform"):
-        """Generate form for publishing to catalog service"""
-        from phoenix.schema import PublishSchema
-        return Form(schema=PublishSchema(), buttons=('publish',), formid=formid)
 
-    def process_publish_form(self, form, job_id):
-        try:
-            controls = self.request.POST.items()
-            appstruct = form.validate(controls)
-            # TODO: publish to catalog
-        except ValidationFailure, e:
-            logger.exception('validation of publish form failed')
-            return dict(form=e.render())
-        except Exception,e:
-            logger.exception("publication failed.")
-            self.session.flash("Publication failed. %s" % e, queue='danger')
-        else:
-            self.session.flash("Publication was successful", queue='success')
-        return HTTPFound(location=self.request.route_path('monitor_details', job_id=job_id, tab='outputs'))
-
-    def generate_upload_form(self, formid="deform"):
-        """Generate form for upload to swift cloud"""
-        from phoenix.schema import UploadSchema
-        return Form(schema = UploadSchema(), buttons=('upload',), formid=formid)
-
-    def process_upload_form(self, form, job_id):
-        try:
-            controls = self.request.POST.items()
-            appstruct = form.validate(controls)
-
-            login = swift.swift_login(self.request,
-                                username = appstruct.get('username'),
-                                password = appstruct.get('password'))
-            swift.swift_upload(self.request,
-                         storage_url = login.get('storage_url'),
-                         auth_token = login.get('auth_token'),
-                         container = appstruct.get('container'),
-                         prefix = appstruct.get('prefix'),
-                         source = appstruct.get('source'))
-        except ValidationFailure, e:
-            logger.exception('validation of upload form failed')
-            return dict(form=e.render())
-        except Exception,e:
-            logger.exception("upload failed.")
-            self.session.flash("Upload failed. %s" % e, queue='danger')
-        else:
-            self.session.flash("Swift upload added to Jobs.", queue='info')
-        return HTTPFound(location=self.request.route_path('monitor_details', job_id=job_id, tab='outputs'))
-    
     @panel_config(name='monitor_outputs', renderer='../templates/panels/monitor_outputs.pt')
     def panel(self):
         job_id = self.session.get('job_id')
-        
-        publish_form = self.generate_publish_form()
-        upload_form = self.generate_upload_form()
-
-        if 'publish' in self.request.POST:
-            return self.process_publish_form(publish_form, job_id)
-        elif 'upload' in self.request.POST:
-            return self.process_upload_form(upload_form, job_id)
 
         items = []
         for output in process_outputs(self.request, job_id).values():
@@ -115,9 +53,7 @@ class Outputs(object):
                 ['output', 'value', ''],
             )
 
-        return dict(grid=grid,
-                    publish_form=publish_form.render(),
-                    upload_form=upload_form.render())
+        return dict(grid=grid)
 
 
 class ProcessOutputsGrid(CustomGrid):
