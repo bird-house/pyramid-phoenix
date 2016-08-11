48acd63bdaea2b37fd73ab67f88657169c195cc5
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-11 17:53:56 +0200

    fixed route_path in solrsearch panels

--- phoenix/solrsearch/panels.py
+++ phoenix/solrsearch/panels.py
@@ -6,11 +6,21 @@ import logging
 logger = logging.getLogger(__name__)
 
 
+def query_path(request):
+    # TODO: this is dirty. solrsearch should be a widget or/and use jquery ajax requests.
+    current_path = request.current_route_path()
+    if current_path.startswith('/wizard'):
+        q_path = 'wizard_solr'
+    else:
+        q_path = 'solrsearch'
+    return q_path
+
+
 @panel_config(name='solrsearch_script', renderer='templates/solrsearch/panels/script.pt')
 def solrsearch_script(context, request):
     query = request.params.get('q', '')
     page = int(request.params.get('page', '0'))
-    return dict(query=query, page=page)
+    return dict(query_path=query_path(request), query=query, page=page)
 
 
 @panel_config(name='solrsearch', renderer='templates/solrsearch/panels/search.pt')
@@ -21,7 +31,9 @@ def solrsearch(context, request):
     source = request.params.get('source')
     tag = request.params.get('tag')
 
-    result = dict(query=query, page=page, category=category, selected_source=source)
+    logger.debug("solrsearch panel context %s", context)
+
+    result = dict(query_path=query_path(request), query=query, page=page, category=category, selected_source=source)
     url = request.registry.settings.get('solr.url')
     result.update(solr_search(url=url, query=query, page=page, category=category, source=source, tag=tag))
     # request.session.flash("Solr service is not available.", queue='danger')
