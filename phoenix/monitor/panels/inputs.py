0c446fefe97c1a8bb69f41c1597c22059c85a94d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-11 20:14:56 +0200

    using media box for inputs

--- phoenix/monitor/panels/inputs.py
+++ phoenix/monitor/panels/inputs.py
@@ -1,7 +1,5 @@
 from pyramid_layout.panel import panel_config
 
-from phoenix.grid import CustomGrid
-
 import logging
 logger = logging.getLogger(__name__)
 
@@ -30,62 +28,33 @@ class Inputs(object):
         self.request = request
         self.session = self.request.session
     
-    @panel_config(name='monitor_inputs', renderer='../templates/monitor/panels/default.pt')
+    @panel_config(name='monitor_inputs', renderer='../templates/monitor/panels/media.pt')
     def panel(self):
         job_id = self.session.get('job_id')
         
         items = []
         for inp in process_inputs(self.request, job_id):
+            dataset = None
+            if self.request.wms_activated and inp.mimeType and 'netcdf' in inp.mimeType:
+                if inp.reference and 'cache' in inp.reference:
+                    dataset = "cache" + inp.reference.split('cache')[1]
+            if inp.mimeType:
+                category = 'ComplexType'
+            else:
+                category = 'LiteralType'
+
             items.append(dict(title=inp.title,
                               abstract=inp.abstract,
                               identifier=inp.identifier,
                               mime_type=inp.mimeType,
                               data=inp.data,
-                              reference=inp.reference))
+                              reference=inp.reference,
+                              dataset=dataset,
+                              category=category))
+
         items = sorted(items, key=lambda item: item['identifier'], reverse=1)
+        return dict(items=items)
 
-        grid = ProcessInputsGrid(
-                self.request,
-                items,
-                ['input', 'value', ''],
-            )
-        return dict(grid=grid)
-
-
-class ProcessInputsGrid(CustomGrid):
-    def __init__(self, request, *args, **kwargs):
-        super(ProcessInputsGrid, self).__init__(request, *args, **kwargs)
-        self.column_formats['input'] = self.input_td
-        self.column_formats['value'] = self.value_td
-        self.column_formats[''] = self.buttongroup_td
-        self.exclude_ordering = self.columns
-
-    def input_td(self, col_num, i, item):
-        return self.render_title_td(
-            title=item.get('title'), 
-            abstract=item.get('abstract'))
-
-    def value_td(self, col_num, i, item):
-        return self.render_td(renderer="value_td.mako",
-                data=item.get('data', []),
-                format=item.get('mime_type'),
-                source=item.get('reference'))
-
-    def buttongroup_td(self, col_num, i, item):
-        from phoenix.utils import ActionButton
-        buttons = []
-        if item.get('reference') is not None:
-            buttons.append( ActionButton('view', title=u'Download', icon="fa fa-download",
-                                         href=item.get('reference', "#"), new_window=True))
-            if self.request.wms_activated and 'netcdf' in item.get('mime_type'):
-                if 'cache' in item.get('reference'):
-                    dataset = "cache" + item.get('reference').split('cache')[1]
-                    buttons.append( ActionButton("mapit", title=u'Show on Map', icon="fa fa-map-marker",
-                                    href=self.request.route_path('map', _query=[('dataset', dataset)])))
-            buttons.append(ActionButton('add_to_cart', title=u'Add to Cart', icon="fa fa-cart-plus",
-                                        href=item.get('reference', "#"), new_window=True))
-        return self.render_buttongroup_td(buttons=buttons)
-    
 
 
 
