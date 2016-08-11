37f108a6dad7ce8a5b30b6b454214c511a6cd7cd
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-11 16:31:27 +0200

    using solrserach panel

---
+++ phoenix/solrsearch/panels.py
@@ -0,0 +1,21 @@
+from pyramid_layout.panel import panel_config
+
+from .search import solr_search
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+@panel_config(name='solrsearch', renderer='templates/solrsearch/panels/search.pt')
+def solrsearch(context, request):
+    query = request.params.get('q', '')
+    page = int(request.params.get('page', '0'))
+    category = request.params.get('category')
+    source = request.params.get('source')
+    tag = request.params.get('tag')
+
+    result = dict(query=query, page=page, category=category, selected_source=source)
+    url = request.registry.settings.get('solr.url')
+    result.update(solr_search(url=url, query=query, page=page, category=category, source=source, tag=tag))
+    # request.session.flash("Solr service is not available.", queue='danger')
+    return result
--- phoenix/solrsearch/views/solrsearch.py
+++ phoenix/solrsearch/views/solrsearch.py
@@ -4,7 +4,7 @@ from pyramid.view import view_config, view_defaults
 from mako.template import Template
 
 from phoenix.views import MyView
-import pysolr
+
 
 import logging
 logger = logging.getLogger(__name__)
@@ -19,62 +19,14 @@ class SolrSearch(MyView):
     def __init__(self, request):
         super(SolrSearch, self).__init__(request, name='solrsearch', title='Solrsearch')
 
-    def solr_search(self, query, page, category, source, tag):
-        rows = 10
-        start = page * rows
-
-        if not query or not query.strip():
-            query = '*:*'
-        try:
-            url = self.request.registry.settings.get('solr.url')
-            solr = pysolr.Solr(url, timeout=10)
-            options = {'start': start,
-                       'rows': rows,
-                       'facet': 'true',
-                       'facet.field': ['category', 'source', 'tags'],
-                       'facet.limit': 300,
-                       'facet.mincount': 1}
-            if tag or category or source:
-                options['fq'] = []
-                if category:
-                    options['fq'].append('category:{0}'.format(category))
-                if source:
-                    options['fq'].append('source:{0}'.format(source))
-                if tag:
-                    options['fq'].append('tags:{0}'.format(tag))
-            results = solr.search(query, **options)
-            sources = results.facets['facet_fields']['source'][::2]
-            tag_values = results.facets['facet_fields']['tags'][::2]
-            tag_counts = results.facets['facet_fields']['tags'][1::2]
-            tags = []
-            for i in xrange(0, len(tag_counts)):
-                if tag_counts[i] == results.hits:
-                    # each dataset has the same tag
-                    continue
-                tags.append(tag_values[i])
-            hits = results.hits
-        except:
-            logger.exception("solr search failed")
-            self.session.flash("Solr service is not available.", queue='danger')
-            results = []
-            sources = []
-            tags = []
-            hits = 0
-        finally:
-            end = min(hits, ((page + 1) * rows))
-        return dict(results=results, sources=sources, tags=tags, hits=hits, start=start+1, end=end)
-
     @view_config(route_name='solrsearch', renderer='../templates/solrsearch/solrsearch.pt')
     def view(self):
         query = self.request.params.get('q', '')
         page = int(self.request.params.get('page', '0'))
-        category = self.request.params.get('category')
-        source = self.request.params.get('source')
-        tag = self.request.params.get('tag')
 
-        result = dict(query=query, page=page, category=category, selected_source=source)
-        result.update(self.solr_search(query=query, page=page, category=category, source=source, tag=tag))
-        result['solrsearch_script'] = solrsearch_script.render(request=self.request, query=query, page=page)
-        return result
+        lm = self.request.layout_manager
+        lm.layout.add_heading('solrsearch')
+
+        return dict(solrsearch_script=solrsearch_script.render(request=self.request, query=query, page=page))
 
 
