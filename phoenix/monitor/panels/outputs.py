a6ba8338035b4435fd932e58857535ce9cbfe4a9
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-11 20:30:52 +0200

    using media template for outputs

--- phoenix/monitor/panels/outputs.py
+++ phoenix/monitor/panels/outputs.py
@@ -1,7 +1,5 @@
 from pyramid_layout.panel import panel_config
 
-from phoenix.grid import CustomGrid
-
 import logging
 logger = logging.getLogger(__name__)
 
@@ -33,66 +31,32 @@ class Outputs(object):
         self.request = request
         self.session = self.request.session
 
-    @panel_config(name='monitor_outputs', renderer='../templates/monitor/panels/outputs.pt')
+    @panel_config(name='monitor_outputs', renderer='../templates/monitor/panels/media.pt')
     def panel(self):
         job_id = self.session.get('job_id')
 
         items = []
         for output in process_outputs(self.request, job_id).values():
+            dataset = None
+            if self.request.wms_activated and output.mimeType and 'netcdf' in output.mimeType:
+                if output.reference and 'wpsoutputs' in output.reference:
+                    dataset = "outputs" + output.reference.split('wpsoutputs')[1]
+            if output.mimeType:
+                category = 'ComplexType'
+            else:
+                category = 'LiteralType'
+
             items.append(dict(title=output.title,
                               abstract=output.abstract,
                               identifier=output.identifier,
                               mime_type=output.mimeType,
                               data=output.data,
-                              reference=output.reference))
+                              reference=output.reference,
+                              dataset=dataset,
+                              category=category))
         items = sorted(items, key=lambda item: item['identifier'], reverse=1)
+        return dict(items=items)
 
-        grid = ProcessOutputsGrid(
-                self.request,
-                items,
-                ['output', 'value', ''],
-            )
-
-        return dict(grid=grid)
-
-
-class ProcessOutputsGrid(CustomGrid):
-    def __init__(self, request, *args, **kwargs):
-        super(ProcessOutputsGrid, self).__init__(request, *args, **kwargs)
-        self.column_formats['output'] = self.output_td
-        self.column_formats['value'] = self.value_td
-        # self.column_formats['preview'] = self.preview_td
-        self.column_formats[''] = self.buttongroup_td
-        self.exclude_ordering = self.columns
-
-    def output_td(self, col_num, i, item):
-        return self.render_title_td(
-            title=item.get('title'), 
-            abstract=item.get('abstract'))
-
-    def value_td(self, col_num, i, item):
-        return self.render_td(renderer="value_td.mako",
-                data=item.get('data', []),
-                format=item.get('mime_type'),
-                source=item.get('reference'))
-
-    def preview_td(self, col_num, i, item):
-        return self.render_preview_td(format=item.get('mime_type'), source=item.get('reference'))
-
-    def buttongroup_td(self, col_num, i, item):
-        from phoenix.utils import ActionButton
-        buttons = []
-        if item.get('reference') is not None:
-            buttons.append( ActionButton('view', title=u'Download', icon="fa fa-download",
-                                         href=item.get('reference', "#"), new_window=True))
-            if self.request.wms_activated and 'netcdf' in item.get('mime_type'):
-                if 'wpsoutputs' in item.get('reference'):
-                    dataset = "outputs" + item.get('reference').split('wpsoutputs')[1]
-                    buttons.append( ActionButton("mapit", title=u'Show on Map', icon="fa fa-map-marker",
-                                    href=self.request.route_path('map', _query=[('dataset', dataset)])))
-            buttons.append(ActionButton('add_to_cart', title=u'Add to Cart', icon="fa fa-cart-plus",
-                                        href=item.get('reference', "#"), new_window=True))
-        return self.render_buttongroup_td(buttons=buttons)
         
 
 
