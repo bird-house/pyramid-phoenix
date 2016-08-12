2de3918d696bb9217e4fdec204f73c4d1da18ee0
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-12 17:57:09 +0200

    handle process exception

--- phoenix/tasks/execute.py
+++ phoenix/tasks/execute.py
@@ -6,54 +6,73 @@ from owslib.wps import WebProcessingService
 
 from phoenix.db import mongodb
 from phoenix.events import JobFinished
-from phoenix.tasks.utils import wps_headers, log, log_error, add_job
+from phoenix.tasks.utils import wps_headers, log, log_error, add_job, wait_secs
 
 from celery.utils.log import get_task_logger
 logger = get_task_logger(__name__)
 
 
 @app.task(bind=True)
-def execute_process(self, userid, url, identifier, inputs, outputs, caption=None):
+def execute_process(self, url, identifier, inputs, outputs, userid=None, caption=None):
     registry = app.conf['PYRAMID_REGISTRY']
     db = mongodb(registry)
+    job = add_job(
+        db,
+        userid=userid,
+        task_id=self.request.id,
+        service=identifier,
+        is_workflow=False,
+        caption=caption)
 
-    wps = WebProcessingService(url=url, skip_caps=False, verify=False, headers=wps_headers(userid))
-    execution = wps.execute(identifier, inputs=inputs, output=outputs, lineage=True)
+    try:
+        wps = WebProcessingService(url=url, skip_caps=False, verify=False, headers=wps_headers(userid))
+        execution = wps.execute(identifier, inputs=inputs, output=outputs, lineage=True)
+        job['service'] = wps.identification.title
+        job['title'] = wps.identification.title
+        job['abstract'] = getattr(execution.process, "abstract")
+        job['status_location'] = execution.statusLocation
 
-    if not userid:
-        userid = 'guest'
+        logger.debug("job init done %s ...", self.request.id)
 
-    job = add_job(db, userid,
-                  task_id=self.request.id,
-                  is_workflow=False,
-                  service=wps.identification.title,
-                  title=identifier,
-                  abstract=getattr(execution.process, "abstract", ""),
-                  caption=caption,
-                  status_location=execution.statusLocation)
+        num_retries = 0
+        run_step = 0
+        while execution.isNotComplete():
+            if num_retries >= 5:
+                raise Exception("Could not read status document after 5 retries. Giving up.")
+            try:
+                execution.checkStatus(sleepSecs=wait_secs(run_step))
+                job['status'] = execution.getStatus()
+                job['status_message'] = execution.statusMessage
+                job['progress'] = execution.percentCompleted
+                duration = datetime.now() - job.get('created', datetime.now())
+                job['duration'] = str(duration).split('.')[0]
 
-    while execution.isNotComplete():
-        try:
-            execution.checkStatus(sleepSecs=3)
-            job['status'] = execution.getStatus()
-            job['status_message'] = execution.statusMessage
-            job['progress'] = execution.percentCompleted
-            duration = datetime.now() - job.get('created', datetime.now())
-            job['duration'] = str(duration).split('.')[0]
-            if execution.isComplete():
-                job['finished'] = datetime.now()
-                if execution.isSucceded():
-                    job['progress'] = 100
-                    log(job)
+                if execution.isComplete():
+                    job['finished'] = datetime.now()
+                    if execution.isSucceded():
+                        job['progress'] = 100
+                        log(job)
+                    else:
+                        job['status_message'] = '\n'.join(error.text for error in execution.errors)
+                        for error in execution.errors:
+                            log_error(job, error)
                 else:
-                    job['status_message'] = '\n'.join(error.text for error in execution.errors)
-                    for error in execution.errors:
-                        log_error(job, error)
+                    log(job)
+            except:
+                num_retries += 1
+                logger.exception("Could not read status xml document for job %s. Trying again ...", self.request.id)
             else:
-                log(job)
-        except:
-            logger.exception("Could not read status xml document.")
-        else:
-            db.jobs.update({'identifier': job['identifier']}, job)
+                logger.debug("update job %s ...", self.request.id)
+                num_retries = 0
+                run_step += 1
+                db.jobs.update({'identifier': job['identifier']}, job)
+    except Exception as exc:
+        logger.exception("Failed to run Job")
+        job['status'] = "ProcessFailed"
+        job['status_message'] = "Failed to run Job. %s" % exc.message
+    finally:
+        log(job)
+        db.jobs.update({'identifier': job['identifier']}, job)
+
     registry.notify(JobFinished(job))
-    return execution.getStatus()
+    return job['status']
