7a311943edf44e8f935bfb700ce0eddead8c0894
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-16 11:15:20 +0200

    pep8

--- phoenix/db.py
+++ phoenix/db.py
@@ -14,7 +14,7 @@ def mongodb(registry):
     settings = registry.settings
     client = pymongo.MongoClient(settings['mongodb.host'], int(settings['mongodb.port']))
     db = client[settings['mongodb.db_name']]
-    #db.services.create_index("name", unique=True)
+    # db.services.create_index("name", unique=True)
     return db
 
 
