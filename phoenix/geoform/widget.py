ab4a7ccdbd275c8ac8e6e91212722e429fc75f46
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-08-17 14:21:52 +0200

    using resource.pt template

--- phoenix/geoform/widget.py
+++ phoenix/geoform/widget.py
@@ -27,47 +27,23 @@ log = logging.getLogger(__name__)
 
 class ResourceWidget(Widget):
     """
-    Renders an ``<input type="text"/>`` widget.
-    **Attributes/Arguments**
-    template
-       The template name used to render the widget.  Default:
-        ``textinput``.
-    readonly_template
-        The template name used to render the widget in read-only mode.
-        Default: ``readonly/textinput``.
-    strip
-        If true, during deserialization, strip the value of leading
-        and trailing whitespace (default ``True``).
-    mask
-        A :term:`jquery.maskedinput` input mask, as a string.
-        a - Represents an alpha character (A-Z,a-z)
-        9 - Represents a numeric character (0-9)
-        * - Represents an alphanumeric character (A-Z,a-z,0-9)
-        All other characters in the mask will be considered mask
-        literals.
-        Example masks:
-          Date: 99/99/9999
-          US Phone: (999) 999-9999
-          US SSN: 999-99-9999
-        When this option is used, the :term:`jquery.maskedinput`
-        library must be loaded into the page serving the form for the
-        mask argument to have any effect.  See :ref:`masked_input`.
-    mask_placeholder
-        The placeholder for required nonliteral elements when a mask
-        is used.  Default: ``_`` (underscore).
+    Renders an WPS ComplexType input widget.
+
+    It is based on the TextInputWidget
     """
-    template = 'textinput'
-    readonly_template = 'readonly/textinput'
+    template = 'resource'
+    # readonly_template = 'readonly/textinput'
     strip = True
-    mask = None
-    mask_placeholder = "_"
+    # mask = None
+    # mask_placeholder = "_"
     requirements = (('jquery.maskedinput', None), )
 
     def serialize(self, field, cstruct, **kw):
         if cstruct in (null, None):
             cstruct = ''
-        readonly = kw.get('readonly', self.readonly)
-        template = readonly and self.readonly_template or self.template
+        # readonly = kw.get('readonly', self.readonly)
+        # template = readonly and self.readonly_template or self.template
+        template = self.template
         values = self.get_template_values(field, cstruct, kw)
         return field.renderer(template, **values)
 
