08b8a07e8090478ecfc3adce6d1b45c504571a99
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-11-07 16:27:47 +0100

    added async parameter to execute_process

--- phoenix/tasks/execute.py
+++ phoenix/tasks/execute.py
@@ -13,7 +13,7 @@ logger = get_task_logger(__name__)
 
 
 @app.task(bind=True)
-def execute_process(self, url, identifier, inputs, outputs, userid=None, caption=None):
+def execute_process(self, url, identifier, inputs, outputs, async=True, userid=None, caption=None):
     registry = app.conf['PYRAMID_REGISTRY']
     db = mongodb(registry)
     job = add_job(
@@ -27,7 +27,7 @@ def execute_process(self, url, identifier, inputs, outputs, userid=None, caption
 
     try:
         wps = WebProcessingService(url=url, skip_caps=False, verify=False, headers=wps_headers(userid))
-        execution = wps.execute(identifier, inputs=inputs, output=outputs, lineage=True)
+        execution = wps.execute(identifier, inputs=inputs, output=outputs, async=async, lineage=True)
         job['service'] = wps.identification.title
         # job['title'] = getattr(execution.process, "title")
         job['abstract'] = getattr(execution.process, "abstract")
@@ -37,7 +37,7 @@ def execute_process(self, url, identifier, inputs, outputs, userid=None, caption
 
         num_retries = 0
         run_step = 0
-        while execution.isNotComplete():
+        while execution.isNotComplete() or not run_step:
             if num_retries >= 5:
                 raise Exception("Could not read status document after 5 retries. Giving up.")
             try:
