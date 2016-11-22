a8aadfc92a77115c08a7b4acec60bb56d2ce2922
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-11-22 15:40:33 +0100

    pep8

--- phoenix/views.py
+++ phoenix/views.py
@@ -17,6 +17,7 @@ from phoenix.utils import save_upload, save_chunk, combine_chunks
 import logging
 logger = logging.getLogger(__name__)
 
+
 class MyView(object):
     def __init__(self, request, name, title, description=None):
         self.request = request
@@ -50,10 +51,12 @@ def notfound(request):
     """
     return {}
 
+
 @subscriber(BeforeRender)
 def add_global(event):
     event['message_type'] = 'alert-info'
     event['message'] = ''
+
 
 @view_config(context=Exception)
 def unknown_failure(request, exc):
@@ -61,9 +64,10 @@ def unknown_failure(request, exc):
     logger.exception('unknown failure')
     #msg = exc.args[0] if exc.args else ""
     #response =  Response('Ooops, something went wrong: %s' % (traceback.format_exc()))
-    response =  Response('Ooops, something went wrong. Check the log files.')
+    response = Response('Ooops, something went wrong. Check the log files.')
     response.status_int = 500
     return response
+
 
 @view_config(route_name='download')
 def download(request):
@@ -71,6 +75,7 @@ def download(request):
     #filename = request.params['filename']
     logger.debug("download: %s", request.storage.path(filename))
     return FileResponse(request.storage.path(filename))
+
 
 def handle_upload(request, attrs):
     """
@@ -87,12 +92,12 @@ def handle_upload(request, attrs):
     request.storage.filename_allowed(attrs['qqfilename'])
 
     # Chunked?
-    if attrs.has_key('qqtotalparts') and int(attrs['qqtotalparts']) > 1:
+    if 'qqtotalparts' in attrs and int(attrs['qqtotalparts']) > 1:
         dest_folder = os.path.join(request.storage.path('chunks'), attrs['qquuid'])
         dest = os.path.join(dest_folder, "parts", str(attrs['qqpartindex']))
         logger.debug('Chunked upload received')
         save_chunk(fs.file, dest)
-        
+
         # If the last chunk has been sent, combine the parts.
         if int(attrs['qqtotalparts']) - 1 == int(attrs['qqpartindex']):
             filename = os.path.join(dest_folder, attrs['qqfilename'])
@@ -100,14 +105,14 @@ def handle_upload(request, attrs):
                 int(attrs['qqtotalparts']),
                 source_folder=os.path.dirname(dest),
                 dest=filename)
-            
+
             save_upload(request, filename=filename)
 
             shutil.rmtree(dest_folder)
-    else: # not chunked
+    else:  # not chunked
         save_upload(request, fs=fs, filename=attrs['qqfilename'])
 
-        
+
 @view_config(route_name='upload', renderer='json', request_method="POST", xhr=True, accept="application/json")
 def upload(request):
     logger.debug("upload post=%s", request.POST)
@@ -126,6 +131,7 @@ def upload(request):
             result = {"success": False, 'error': msg}
     return result
 
+
 @view_defaults(permission='view', layout='default')
 class Home(object):
     def __init__(self, request):
@@ -135,6 +141,3 @@ class Home(object):
     @view_config(route_name='home', renderer='phoenix:templates/home.pt')
     def view(self):
         return {}
-
-
-
