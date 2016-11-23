bfd5537a1c242d62184cb9e05ba866f6383fbcd4
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-11-23 14:07:40 +0100

    simplified monitor details view

--- phoenix/monitor/views/details.py
+++ phoenix/monitor/views/details.py
@@ -12,12 +12,6 @@ class Details(MyView):
         super(Details, self).__init__(
             request, name='monitor_details', title='Details')
 
-    def breadcrumbs(self):
-        breadcrumbs = super(Details, self).breadcrumbs()
-        breadcrumbs.append(dict(route_path=self.request.route_path('monitor'), title='Monitor'))
-        breadcrumbs.append(dict(route_path='', title=self.title))
-        return breadcrumbs
-
     @view_config(route_name='monitor_details', renderer='../templates/monitor/details.pt')
     def view(self):
         tab = self.request.matchdict.get('tab')
