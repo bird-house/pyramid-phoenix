a2add303efdedde7deee0ddf17a7038f561e7acf
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-12-05 19:02:59 +0100

    pep8

--- phoenix/events.py
+++ phoenix/events.py
@@ -3,9 +3,10 @@ class JobStarted(object):
         self.request = request
         self.task_id = task_id
 
+
 class JobFinished(object):
     def __init__(self, job):
         self.job = job
-        
+
     def succeeded(self):
         return self.job.get('status') == "ProcessSucceeded"
