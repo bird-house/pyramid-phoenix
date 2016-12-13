8481c5432187280781ec4b2aaca125fe625b2643
Carsten Ehbrecht <ehbrecht@dkrz.de>
2016-12-13 19:10:07 +0100

    working on solr activation

--- phoenix/solr/__init__.py
+++ phoenix/solr/__init__.py
@@ -3,17 +3,18 @@ from pyramid.settings import asbool
 import logging
 logger = logging.getLogger(__name__)
 
+
 def includeme(config):
     settings = config.registry.settings
 
-    logger.info('Adding solr ...')
-
-    # actions
+    if asbool(settings.get('phoenix.solr', 'false')):
+        # actions
+        pass
     config.add_route('index_service', '/solr/{service_id}/index')
     config.add_route('clear_index', '/solr/clear')
-    
+
     # check if solr is activated
     def solr_activated(request):
         settings = request.registry.settings
-        return asbool(settings.get('phoenix.solr', True))
+        return asbool(settings.get('phoenix.solr', 'false'))
     config.add_request_method(solr_activated, reify=True)
