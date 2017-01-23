3e4555db9ab5c035a8a69f5a217dbd5b999e64d5
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-01-23 15:19:00 +0100

    fixed request.wps using settings

--- phoenix/wizard/__init__.py
+++ phoenix/wizard/__init__.py
@@ -29,20 +29,20 @@ def includeme(config):
         # add malleefowl wps
         def get_wps(request):
             settings = request.registry.settings
-            session = request.session
-            if 'wps' not in session:
+            if 'wps' not in settings:
                 logger.debug('register malleefowl wps service')
                 try:
                     service_name = 'malleefowl'
                     registry = twitcher_service_factory(request.registry)
                     logger.debug("register: name=%s, url=%s", service_name, settings['wps.url'])
                     registry.register_service(url=settings['wps.url'], data={'name': service_name}, overwrite=True)
-                    session['wps'] = WebProcessingService(
+                    settings['wps'] = WebProcessingService(
                         url=request.route_url('owsproxy', service_name=service_name),
                         skip_caps=True, verify=False)
+                    logger.debug('malleefowl proxy url: %s', settings['wps'].url)
                 except:
                     logger.exception('Could not connect malleefowl wps %s', settings['wps.url'])
-            return session.get('wps')
+            return settings.get('wps')
         config.add_request_method(get_wps, 'wps', reify=True)
 
     # check if wizard is activated
