46a3697689b1d8f68f8b08e2dae2a3308d4c8bc9
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-02-22 16:52:15 +0100

    update icons of esgfsearch

--- phoenix/panels.py
+++ phoenix/panels.py
@@ -29,9 +29,9 @@ def navbar(context, request):
     subitems.append(nav_item('Dashboard', request.route_path('dashboard', tab='overview'), icon='fa fa-dashboard'))
     # dropdown browse
     browse_items = list()
-    browse_items.append(nav_item('ESGF search', request.route_path('esgfsearch'), icon='fa fa-search'))
+    browse_items.append(nav_item('ESGF search', request.route_path('esgfsearch'), icon='fa fa-globe'))
     if request.solr_activated:
-        browse_items.append(nav_item('Solr search', request.route_path('solrsearch'), icon='fa fa-search'))
+        browse_items.append(nav_item('Birdhouse Solr', request.route_path('solrsearch'), icon='fa fa-sun-o'))
     subitems.append(dropdown('Browse', items=browse_items, icon='fa fa-search'))
     if request.has_permission('submit'):
         subitems.append(nav_item('Cart', request.route_path('cart'), icon='fa fa-shopping-cart'))
