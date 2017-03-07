c415961740b75f4fd1c6abc181a4ae4b9e4c2033
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-03-07 13:11:14 +0100

    update esgfsearch quick view

--- phoenix/esgfsearch/views/__init__.py
+++ phoenix/esgfsearch/views/__init__.py
@@ -1,6 +1,8 @@
 from pyramid.view import view_defaults
+from deform import Form
 
 from phoenix.esgfsearch.schema import ESGFSearchSchema
+from phoenix.esgfsearch.search import search
 
 
 @view_defaults(permission='view', layout='default')
@@ -9,4 +11,17 @@ class ESGFSearch(object):
         self.request = request
 
     def view(self):
-        return dict()
+        result = dict(
+            query=self.request.params.get('query', ''),
+            selected=self.request.params.get('selected', 'project'),
+            distrib=self.request.params.get('distrib', 'false'),
+            replica=self.request.params.get('replica', 'false'),
+            latest=self.request.params.get('latest', 'true'),
+            temporal=self.request.params.get('temporal', 'true'),
+            start=self.request.params.get('start', '2001'),
+            end=self.request.params.get('end', '2005'),
+            constraints=self.request.params.get('constraints', ''),
+        )
+        result.update(search(self.request))
+        result.update(form=Form(ESGFSearchSchema()))
+        return result
