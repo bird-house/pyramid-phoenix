b0a55077d8566f64d2d478b48dce26b87af90456
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-03-17 16:57:32 +0100

    cleaned up temporal variable

--- phoenix/esgfsearch/__init__.py
+++ phoenix/esgfsearch/__init__.py
@@ -2,7 +2,7 @@ def includeme(config):
     config.add_route('esgfsearch', '/esgfsearch')
     config.add_view('phoenix.esgfsearch.views.ESGFSearch',
                     route_name='esgfsearch',
-                    attr='view',
+                    attr='search_datasets',
                     renderer='templates/esgfsearch/esgfsearch.pt')
 
     config.add_route('esgfsearch_files', '/esgfsearch/files')
--- phoenix/esgfsearch/views/__init__.py
+++ phoenix/esgfsearch/views/__init__.py
@@ -25,20 +25,21 @@ class ESGFSearch(object):
         self.replica = asbool(request.params.get('replica', 'false'))
         if self.replica is True:
             self.replica = None  # master + replica
+        if 'start' in self.request.params and 'end' in self.request.params:
+            self.temporal = True
+        else:
+            self.temporal = False
         self.conn = SearchConnection(settings.get('esgfsearch.url'), distrib=distrib)
 
     def search_files(self):
         dataset_id = self.request.params.get(
             'dataset_id',
             'cmip5.output1.MPI-M.MPI-ESM-LR.1pctCO2.day.atmos.cfDay.r1i1p1.v20120314|esgf1.dkrz.de')
-        if 'start' in self.request.params:
+        if self.temporal:
             start = int(self.request.params['start'])
-        else:
-            start = None
-        if 'end' in self.request.params:
             end = int(self.request.params['end'])
         else:
-            end = None
+            start = end = None
         ctx = self.conn.new_context(search_type=TYPE_FILE, latest=self.latest, replica=self.replica)
         ctx = ctx.constrain(dataset_id=dataset_id)
         paged_results = []
@@ -52,18 +53,14 @@ class ESGFSearch(object):
                 ))
         return dict(files=paged_results)
 
-    def view(self):
-        if 'start' in self.request.params and 'end' in self.request.params:
-            temporal = 'true'
-        else:
-            temporal = 'false'
+    def search_datasets(self):
         result = dict(
             query=self.request.params.get('query', ''),
             selected=self.request.params.get('selected', 'project'),
             distrib=self.request.params.get('distrib', 'false'),
             replica=self.request.params.get('replica', 'false'),
             latest=self.request.params.get('latest', 'true'),
-            temporal=temporal,
+            temporal=str(self.temporal).lower(),
             start=self.request.params.get('start', '2001'),
             end=self.request.params.get('end', '2005'),
             constraints=self.request.params.get('constraints', ''),
