7b98b55ebe3e1de7c4f767a75d5cfab294a79e3b
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-03-22 14:27:16 +0100

    toggle cart button

--- phoenix/static/phoenix/js/cart.js
+++ phoenix/static/phoenix/js/cart.js
@@ -5,35 +5,32 @@ $(function() {
   // ----------------------------------------------
 
   $(document.body).on('click', '.btn-cart-add' ,function() {
-  //$('.btn-cart-add').on('click', function (event) {
     var url = $(this).attr('data-value');
-    //var btn = $(this);
+    var btn = $(this);
     // call json
     $.getJSON(
       '/add_to_cart.json', {'url': url}, function(json) {
-        // TODO: this can also be done with jquery
-        location.reload();
-        // btn.attr('class', "btn btn-default btn-xs pull-right btn-cart-remove");
-        // btn.attr('title', "Remove from Cart");
-        // $("> icon", btn).attr('class', 'fa fa-lg fa-times');
+        btn.removeClass('btn-cart-add');
+        btn.addClass('btn-cart-remove');
+        btn.attr('title', "Remove from Cart");
+        $("> icon", btn).attr('class', 'fa fa-lg fa-times');
       }
     );
   })
 
   // Remove url from cart
   // ----------------------------------------------
+
   $(document.body).on('click', '.btn-cart-remove' ,function() {
-  //$('.btn-cart-remove').on('click', function (event) {
     var url = $(this).attr('data-value');
-    //var btn = $(this)
+    var btn = $(this);
     // call json
     $.getJSON(
       '/remove_from_cart.json', {'url': url}, function(json) {
-        // TODO: this can also be done with jquery
-        location.reload();
-        // btn.attr('class', "btn btn-default btn-xs pull-right btn-cart-add");
-        // btn.attr('title', "Add to Cart");
-        // $("> icon", btn).attr('class', 'fa fa-lg fa-cart-plus');
+        btn.removeClass('btn-cart-remove');
+        btn.addClass('btn-cart-add');
+        btn.attr('title', "Add to Cart");
+        $("> icon", btn).attr('class', 'fa fa-lg fa-cart-plus');
       }
     );
   })
