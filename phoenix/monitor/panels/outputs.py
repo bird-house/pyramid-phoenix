0ded3f230f386005de0c3af9a01e1abc7f3a5224
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-03-30 17:05:49 +0200

    handling bbox output in monitor

--- phoenix/monitor/panels/outputs.py
+++ phoenix/monitor/panels/outputs.py
@@ -51,8 +51,13 @@ class Outputs(object):
                     logger.debug("proxy reference: %s", proxy_reference)
             if output.mimeType:
                 category = 'ComplexType'
+                data = output.data
+            elif output.dataType == 'BoundingBoxData':
+                category = 'BoundingBoxType'
+                data = ["{0.minx},{0.miny},{0.maxx},{0.maxy}".format(bbox) for bbox in output.data]
             else:
                 category = 'LiteralType'
+                data = output.data
 
             logger.debug("proxy_reference: %s", proxy_reference)
 
@@ -60,7 +65,7 @@ class Outputs(object):
                               abstract=output.abstract,
                               identifier=output.identifier,
                               mime_type=output.mimeType,
-                              data=output.data,
+                              data=data,
                               reference=output.reference,
                               proxy_reference=proxy_reference,
                               dataset=dataset,
