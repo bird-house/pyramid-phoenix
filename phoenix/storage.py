ab70cefda15e06e5cc9fad2f5064063a56554de6
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-04-24 10:57:13 +0200

    update storage

--- phoenix/storage.py
+++ phoenix/storage.py
@@ -1,12 +1,12 @@
 import os
 
 import logging
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
 
 def includeme(config):
     # settings = config.registry.settings
-    logger.debug('Adding storage ...')
+    LOGGER.debug('Adding storage ...')
 
     config.include('pyramid_storage')
     config.add_route('download_storage', 'download/storage/{filename:.*}')
@@ -16,15 +16,15 @@ def includeme(config):
 
 
 def save_upload(request, filename, fs=None):
-    logger.debug("save_upload: filename=%s, fs=%s", filename, fs)
+    LOGGER.debug("save_upload: filename=%s, fs=%s", filename, fs)
     if request.storage.exists(os.path.basename(filename)):
         request.storage.delete(os.path.basename(filename))
     if fs is None:
         stored_filename = request.storage.save_filename(filename)
-        logger.debug('saved chunked file to upload storage %s', stored_filename)
+        LOGGER.debug('saved chunked file to upload storage %s', stored_filename)
     else:
         stored_filename = request.storage.save_file(fs.file, filename=filename)
-        logger.debug('saved file to upload storage %s', stored_filename)
+        LOGGER.debug('saved file to upload storage %s', stored_filename)
 
 
 def save_chunk(fs, path):
@@ -47,7 +47,7 @@ def combine_chunks(total_parts, source_folder, dest):
     Chunks are stored in chunks/
     """
 
-    logger.debug("Combining chunks: %s", source_folder)
+    LOGGER.debug("Combining chunks: %s", source_folder)
 
     if not os.path.exists(os.path.dirname(dest)):
         os.makedirs(os.path.dirname(dest))
@@ -57,4 +57,4 @@ def combine_chunks(total_parts, source_folder, dest):
             part = os.path.join(source_folder, str(i))
             with open(part, 'rb') as source:
                 destination.write(source.read())
-        logger.debug("Combined: %s", dest)
+        LOGGER.debug("Combined: %s", dest)
