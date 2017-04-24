2ee114da9d716be617a2a835bebaf4f85231c473
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-04-24 14:34:54 +0200

    cleaned up storage ... using uuid in path

--- phoenix/storage/__init__.py
+++ phoenix/storage/__init__.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 
 import logging
 LOGGER = logging.getLogger(__name__)
@@ -11,50 +12,3 @@ def includeme(config):
     config.include('pyramid_storage')
     config.add_route('download_storage', 'download/storage/{filename:.*}')
     config.add_route('upload', 'upload')
-
-# upload helpers
-
-
-def save_upload(request, filename, fs=None):
-    LOGGER.debug("save_upload: filename=%s, fs=%s", filename, fs)
-    if request.storage.exists(os.path.basename(filename)):
-        request.storage.delete(os.path.basename(filename))
-    if fs is None:
-        stored_filename = request.storage.save_filename(filename)
-        LOGGER.debug('saved chunked file to upload storage %s', stored_filename)
-    else:
-        stored_filename = request.storage.save_file(fs.file, filename=filename)
-        LOGGER.debug('saved file to upload storage %s', stored_filename)
-
-
-def save_chunk(fs, path):
-    """
-    Save an uploaded chunk.
-
-    Chunks are stored in chunks/
-    """
-    if not os.path.exists(os.path.dirname(path)):
-        os.makedirs(os.path.dirname(path))
-    with open(path, 'wb+') as destination:
-        destination.write(fs.read())
-
-
-def combine_chunks(total_parts, source_folder, dest):
-    """
-    Combine a chunked file into a whole file again. Goes through each part
-    , in order, and appends that part's bytes to another destination file.
-
-    Chunks are stored in chunks/
-    """
-
-    LOGGER.debug("Combining chunks: %s", source_folder)
-
-    if not os.path.exists(os.path.dirname(dest)):
-        os.makedirs(os.path.dirname(dest))
-
-    with open(dest, 'wb+') as destination:
-        for i in xrange(int(total_parts)):
-            part = os.path.join(source_folder, str(i))
-            with open(part, 'rb') as source:
-                destination.write(source.read())
-        LOGGER.debug("Combined: %s", dest)
--- phoenix/storage/views.py
+++ phoenix/storage/views.py
@@ -8,8 +8,6 @@ from pyramid.response import FileResponse
 
 from pyramid_storage.exceptions import FileNotAllowed
 
-from phoenix.storage import save_upload, save_chunk, combine_chunks
-
 import logging
 LOGGER = logging.getLogger(__name__)
 
@@ -17,9 +15,21 @@ LOGGER = logging.getLogger(__name__)
 @view_config(route_name='download_storage')
 def download(request):
     filename = request.matchdict.get('filename')
-    #filename = request.params['filename']
-    LOGGER.debug("download: %s", request.storage.path(filename))
     return FileResponse(request.storage.path(filename))
+
+
+@view_config(route_name='upload', renderer='json', request_method="POST", xhr=True, accept="application/json")
+def upload(request):
+    result = {"success": False}
+    if 'qqfile' in request.POST:
+        try:
+            handle_upload(request, request.POST)
+            result = {'success': True, 'uuid': request.POST['qquuid']}
+        except FileNotAllowed:
+            result = {"success": False, 'error': "Filename extension not allowed", "preventRetry": True}
+        except Exception:
+            result = {"success": False, 'error': "Upload failed"}
+    return result
 
 
 def handle_upload(request, attrs):
@@ -40,38 +50,54 @@ def handle_upload(request, attrs):
     if 'qqtotalparts' in attrs and int(attrs['qqtotalparts']) > 1:
         dest_folder = os.path.join(request.storage.path('chunks'), attrs['qquuid'])
         dest = os.path.join(dest_folder, "parts", str(attrs['qqpartindex']))
-        LOGGER.debug('Chunked upload received')
         save_chunk(fs.file, dest)
 
         # If the last chunk has been sent, combine the parts.
         if int(attrs['qqtotalparts']) - 1 == int(attrs['qqpartindex']):
-            filename = os.path.join(dest_folder, attrs['qqfilename'])
+            filename = os.path.join(dest_folder, attrs['qquuid'], attrs['qqfilename'])
             combine_chunks(
                 int(attrs['qqtotalparts']),
                 source_folder=os.path.dirname(dest),
                 dest=filename)
-
-            save_upload(request, filename=filename)
-
+            save_upload(request, filename=filename, folder=attrs['qquuid'])
             shutil.rmtree(dest_folder)
     else:  # not chunked
-        save_upload(request, fs=fs, filename=attrs['qqfilename'])
+        save_upload(request, fs=fs, filename=attrs['qqfilename'], folder=attrs['qquuid'])
 
 
-@view_config(route_name='upload', renderer='json', request_method="POST", xhr=True, accept="application/json")
-def upload(request):
-    LOGGER.debug("upload post=%s", request.POST)
-    result = {"success": False}
-    if 'qqfile' in request.POST:
-        try:
-            handle_upload(request, request.POST)
-            result = {'success': True}
-        except FileNotAllowed:
-            msg = "Filename extension not allowed"
-            LOGGER.warn(msg)
-            result = {"success": False, 'error': msg, "preventRetry": True}
-        except Exception:
-            msg = "Upload failed"
-            LOGGER.exception(msg)
-            result = {"success": False, 'error': msg}
-    return result
+def save_upload(request, filename, fs=None, folder=None):
+    LOGGER.debug("save: filename=%s, fs=%s", filename, fs)
+    if not fs:
+        stored_filename = request.storage.save_filename(filename, folder=folder)
+    else:
+        stored_filename = request.storage.save_file(fs.file, filename=filename, folder=folder)
+    return stored_filename
+
+
+def save_chunk(fs, path):
+    """
+    Save an uploaded chunk.
+
+    Chunks are stored in chunks/
+    """
+    if not os.path.exists(os.path.dirname(path)):
+        os.makedirs(os.path.dirname(path))
+    with open(path, 'wb+') as destination:
+        destination.write(fs.read())
+
+
+def combine_chunks(total_parts, source_folder, dest):
+    """
+    Combine a chunked file into a whole file again. Goes through each part,
+    in order, and appends that part's bytes to another destination file.
+
+    Chunks are stored in chunks/
+    """
+    if not os.path.exists(os.path.dirname(dest)):
+        os.makedirs(os.path.dirname(dest))
+
+    with open(dest, 'wb+') as destination:
+        for i in xrange(int(total_parts)):
+            part = os.path.join(source_folder, str(i))
+            with open(part, 'rb') as source:
+                destination.write(source.read())
