1e1e22c2da879817acbfb9c24beb19253d0e75ba
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-04-25 20:26:58 +0200

    refactored job details

--- phoenix/monitor/views/details.py
+++ phoenix/monitor/views/details.py
@@ -10,11 +10,11 @@ logger = logging.getLogger(__name__)
 class Details(MyView):
     def __init__(self, request):
         super(Details, self).__init__(request, name='job_details', title='Job Details')
+        self.tab = self.request.matchdict.get('tab')
+        self.job_id = self.request.matchdict.get('job_id')
+        self.collection = self.request.db.jobs
 
     @view_config(route_name='job_details', renderer='../templates/monitor/details.pt')
     def view(self):
-        tab = self.request.matchdict.get('tab')
-        job_id = self.request.matchdict.get('job_id')
-        collection = self.request.db.jobs
-        job = collection.find_one({'identifier': job_id})
-        return dict(active=tab, job_id=job_id, status=job['status'])
+        job = self.collection.find_one({'identifier': self.job_id})
+        return dict(active=self.tab, job_id=self.job_id, status=job['status'])
