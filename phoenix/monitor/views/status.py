47ca18b3484f4f113c008c5337591add7ebc242d
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-04-27 15:48:30 +0200

    fixed proxy reference etc ...

--- phoenix/monitor/views/status.py
+++ phoenix/monitor/views/status.py
@@ -32,11 +32,11 @@ class JobStatus(MyView):
                     if output.identifier == 'output':
                         break
                 details = output_details(self.request, output)
-                if details.proxy_reference:
+                if details.get('proxy_reference'):
                     result = '<a href="{0}" class="btn btn-success btn-xs" target="_blank">Show Output</a>'.format(
-                        details.proxy_reference)
+                        details['proxy_reference'])
                 else:
-                    result = '<strong>{0}</strong>'.format(', '.join(details.data))
+                    result = '<strong>{0}</strong>'.format(', '.join(details.get('data', '')))
                 msg = '<h4>Job Succeeded: {1} <a href="{0}" class="btn btn-info btn-xs"> Details</a></h4>'
                 url = self.request.route_path('job_details', tab='outputs', job_id=self.job_id)
                 self.session.flash(msg.format(url, result), queue="success")
@@ -44,6 +44,21 @@ class JobStatus(MyView):
                 msg = '<h4>Job Failed [{0}/100]</h4>'
                 self.session.flash(msg.format(progress), queue="danger")
             else:
-                msg = '<h4><img src="/static/phoenix/img/ajax-loader.gif"></img> Job Running [{0}/100]</h4>'  # noqa
+                msg = '<h4><i class="fa fa-cog fa-spin text-muted fa-lg"></i> Job Running [{0}/100]</h4>'
+                # msg = """
+                # <div class="row">
+                #     <div class="col-md-3"
+                #         <h3>
+                #             <i class="fa fa-cog fa-spin text-muted fa-lg"></i>
+                #             Job Running
+                #             <div class="progress" data-toggle="tooltip" title="Job progress.">
+                #                 <div class="progress-bar progress-bar-striped active" role="progressbar"
+                #                      aria-valuenow="{0}"
+                #                      aria-valuemin="0" aria-valuemax="100" style="min-width: 2em; width: {0}%;">
+                #                 <span>{0}% Complete</span>
+                #             </div>
+                #         </h3>
+                #     </div>
+                # </div>"""
                 self.session.flash(msg.format(progress), queue="warning")
         return dict(status=status, log=log)
