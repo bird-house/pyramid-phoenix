701e90ef7849f007b8028584f061bfcccc9eafb9
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-05-03 18:39:39 +0200

    removed unused github class

--- phoenix/account/__init__.py
+++ phoenix/account/__init__.py
@@ -5,9 +5,8 @@ LOGGER = logging.getLogger("PHOENIX")
 def includeme(config):
     settings = config.registry.settings
 
-    config.add_route('sign_in', '/account/sign_in')
+    config.add_route('sign_in', '/account/login')
     config.add_route('esgf_login', '/account/login/esgf')
-    config.add_route('github_login', '/account/login/github')
     config.add_route('ldap_login', '/account/login/ldap')
     config.add_route('account_logout', '/account/logout')
     config.add_route('account_auth', '/account/auth/{provider}')
