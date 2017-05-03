bf7723c8faf48d67777c02b5280e3a1a505c5fc9
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-05-03 16:02:23 +0200

    using esgf login class

--- phoenix/account/__init__.py
+++ phoenix/account/__init__.py
@@ -6,6 +6,7 @@ def includeme(config):
     settings = config.registry.settings
 
     config.add_route('sign_in', '/account/sign_in')
+    config.add_route('esgf_login', '/account/login/esgf')
     config.add_route('account_login', '/account/login/{protocol}')
     config.add_route('account_logout', '/account/logout')
     config.add_route('account_auth', '/account/auth/{provider_name}')
