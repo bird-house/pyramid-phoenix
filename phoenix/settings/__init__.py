32b06eee71c2eceb7bac0106323aefbc0e4c13b1
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-05-04 13:08:14 +0200

    update logger

--- phoenix/settings/__init__.py
+++ phoenix/settings/__init__.py
@@ -3,13 +3,13 @@ from pyramid.events import NewRequest
 from phoenix.events import SettingsChanged
 
 import logging
-logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger("PHOENIX")
 
 
 def includeme(config):
     settings = config.registry.settings
 
-    logger.debug('Enable settings ...')
+    LOGGER.debug('Enable settings ...')
 
     config.add_route('settings', '/settings')
     config.add_route('settings_auth', '/settings/auth')
@@ -22,16 +22,16 @@ def includeme(config):
     def add_github(event):
         settings = event.request.registry.settings
         if settings.get('_github', False) is False:
-            logger.debug("init github settings")
+            LOGGER.debug("init github settings")
             stored_settings = event.request.db.settings.find_one()
             stored_settings = stored_settings or {}
             if settings.get('github.client.id'):
-                logger.debug('update github settings in database')
+                LOGGER.debug('update github settings in database')
                 stored_settings['github_client_id'] = settings['github.client.id']
                 stored_settings['github_client_secret'] = settings['github.client.secret']
                 event.request.db.settings.save(stored_settings)
             else:
-                logger.debug('update github settings from database')
+                LOGGER.debug('update github settings from database')
                 settings['github.client.id'] = stored_settings.get('github_client_id')
                 settings['github.client.secret'] = stored_settings.get('github_client_secret')
                 # TODO: backward compatibility ... please remove!
@@ -44,17 +44,17 @@ def includeme(config):
     def add_esgf(event):
         settings = event.request.registry.settings
         if settings.get('_esgf', False) is False:
-            logger.debug("init esgf settings")
+            LOGGER.debug("init esgf settings")
             stored_settings = event.request.db.settings.find_one()
             stored_settings = stored_settings or {}
             if settings.get('esgf.slcs.client.id'):
-                logger.debug('update esgf settings in database')
+                LOGGER.debug('update esgf settings in database')
                 stored_settings['esgf_slcs_url'] = settings.get('esgf.slcs.url')
                 stored_settings['esgf_slcs_client_id'] = settings.get('esgf.slcs.client.id')
                 stored_settings['esgf_slcs_client_secret'] = settings.get('esgf.slcs.client.secret')
                 event.request.db.settings.save(stored_settings)
             else:
-                logger.debug('update esgf settings from database')
+                LOGGER.debug('update esgf settings from database')
                 settings['esgf.slcs.url'] = stored_settings.get('esgf_slcs_url')
                 settings['esgf.slcs.client.id'] = stored_settings.get('esgf_slcs_client_id')
                 settings['esgf.slcs.client.secret'] = stored_settings.get('esgf_slcs_client_secret')
@@ -64,10 +64,10 @@ def includeme(config):
     def add_solr(event):
         settings = event.request.registry.settings
         if settings.get('_solr', False) is False:
-            logger.debug("init solr settings")
+            LOGGER.debug("init solr settings")
             stored_settings = event.request.db.settings.find_one()
             stored_settings = stored_settings or {}
-            logger.debug('update solr settings from database')
+            LOGGER.debug('update solr settings from database')
             settings['solr.maxrecords'] = stored_settings.get('solr_maxrecords', -1)
             settings['solr.depth'] = stored_settings.get('solr_depth', 2)
             settings['_solr'] = True
