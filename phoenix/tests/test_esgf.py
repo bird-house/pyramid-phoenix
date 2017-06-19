cdafaddeccc1d6bb999604204b2de8abf85997ae
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-06-19 18:02:20 +0200

    added test for variable filter

--- phoenix/tests/test_esgf.py
+++ phoenix/tests/test_esgf.py
@@ -32,3 +32,10 @@ def test_temporal_filter():
     assert search.temporal_filter(
         "tas_EUR-44i_ECMWF-ERAINT_evaluation_r1i1p1_HMS-ALADIN52_v1_mon_200101-200812.nc",
         1990, 2000) is False
+
+
+def test_variable_filter():
+    c_dict = search.build_constraint_dict(
+        'variable:tas,variable:tasmax,variable:tasmin')
+    assert search.variable_filter(c_dict, {'variable': ['tas']}) is True
+    assert search.variable_filter(c_dict, {'variable': ['pr']}) is False
