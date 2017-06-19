f68c831d0097502dda242a62583f9b42b73847d6
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-06-19 17:43:42 +0200

    added test for date_form_filename

--- phoenix/tests/test_esgf.py
+++ phoenix/tests/test_esgf.py
@@ -12,3 +12,9 @@ def test_build_constraints_dict():
     c_dict = search.build_constraint_dict(
         'project:CMIP5,time_frequency:mon,variable:tas,variable:tasmax,variable:tasmin')
     assert c_dict.getall('variable') == ['tas', 'tasmax', 'tasmin']
+
+
+def test_date_from_filename():
+    date = search.date_from_filename(
+        "tas_EUR-44i_ECMWF-ERAINT_evaluation_r1i1p1_HMS-ALADIN52_v1_mon_200101-200812.nc")
+    assert date == (2001, 2008)
