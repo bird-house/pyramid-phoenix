22f0c2b0e352c3997aa6e9a193b3a500fdbb386f
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-06-20 14:36:22 +0200

    refactored esgf tests

--- phoenix/tests/test_esgf_search.py
+++ phoenix/tests/test_esgf_search.py
@@ -1,11 +1,4 @@
-from phoenix.esgf import metadata
 from phoenix.esgf import search
-
-
-def test_convert_constraints():
-    converted = metadata.convert_constraints(
-        "http://esgf-data.dkrz.de/esg-search/search?project=CMIP5&time_frequency=mon&variable=tas,tasmax,tasmin")  # noqa
-    assert converted == 'project:CMIP5,time_frequency:mon,variable:tas,variable:tasmax,variable:tasmin'
 
 
 def test_build_constraints_dict():
