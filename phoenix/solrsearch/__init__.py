d6b099a38fe44d5a59b4895f89aa09935bfab924
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-06-23 17:42:57 +0200

    fixed solr include

--- phoenix/solrsearch/__init__.py
+++ phoenix/solrsearch/__init__.py
@@ -2,5 +2,6 @@ from pyramid.settings import asbool
 
 
 def includeme(config):
+    settings = config.registry.settings
     if asbool(settings.get('phoenix.solr', 'false')):
         config.add_route('solrsearch', '/solrsearch')
