0e684ee9236ec176090c7e6f37f190f91eba4f88
antony-wilson <antony.wilson@stfc.ac.uk>
2020-11-24 10:57:17 +0000

    update ceda security module

--- phoenix/ceda_security.py
+++ phoenix/ceda_security.py
@@ -2,27 +2,30 @@ import logging
 LOGGER = logging.getLogger("PHOENIX")
 
 
+PROCESS_ROLE_MAPPING = {
+    "GetWeatherStations": ["surface"],
+    "ExtractUKStationData": ["surface"]
+    }
+
+
 def check_ceda_permissions(user, processid):
     if user is None:
         # the user is not logged in so we cannot check
         return False
 
-    login_id = user.get("login_id")
-
-    if processid == "GetWeatherStations":
-        return _check_get_weather_stations(login_id)
-    elif processid == "ExtractUKStationData":
-        return _check_extract_UK_station_data(login_id)
-
-    # the process is not a protected CEDA process
-    return True
-
-
-def _check_get_weather_stations(login_id):
-    if login_id == "user01":
+    if processid not in PROCESS_ROLE_MAPPING.keys():
+        # the process is not a protected CEDA process
         return True
 
+    users_roles = _get_user_roles(user.get("login_id"))
 
-def _check_extract_UK_station_data(login_id):
-    if login_id == "user01":
-        return False
+    for role in users_roles:
+        if role in PROCESS_ROLE_MAPPING[processid]:
+            return True
+
+    return False
+
+
+def _get_user_roles(login_id):
+    # add database lookup here
+    return ["surface", "top"]
