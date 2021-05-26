dd5d3d54bdf67fca9f80c46ef1537b8afeb3aa49
antony-wilson <antony.wilson@stfc.ac.uk>
2021-05-26 10:41:09 +0100

    Get phoenix to read metalink document to provide direct download links to NC files
    #26

--- phoenix/monitor/utils.py
+++ phoenix/monitor/utils.py
@@ -1,4 +1,6 @@
 from pyramid.compat import escape
+from urllib.request import urlopen
+from lxml import etree
 
 import logging
 LOGGER = logging.getLogger("PHOENIX")
@@ -32,3 +34,34 @@ def output_details(request, output):
                 data=escape_output(data),
                 reference=escape_output(output.reference),
                 category=category)
+
+
+def output_details_from_metlink(output):
+    outputs = []
+    with urlopen(output.reference) as response:
+        html = response.read()
+
+    xml = etree.fromstring(html)
+
+    for child in xml:
+        if child.tag.endswith("file"):
+            for file_element in child:
+                if file_element.tag.endswith("identity"):
+                    title = file_element.text
+                elif file_element.tag.endswith("metaurl"):
+                    reference = file_element.text
+                    mime_type = file_element.attrib["mediatype"]
+
+            outputs.append(
+                dict(
+                    title=title,
+                    abstract=f"{title} extracted from {output.title}",
+                    identifier=output.identifier,
+                    mime_type=mime_type,
+                    data=[],
+                    reference=escape_output(reference),
+                    category="ComplexType",
+                )
+            )
+
+    return outputs
