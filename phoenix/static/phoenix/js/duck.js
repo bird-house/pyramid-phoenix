b54d0a543e21d9589131397b2fd54cf649b0181e
Carsten Ehbrecht <ehbrecht@dkrz.de>
2023-05-05 20:42:36 +0200

    update duck search

--- phoenix/static/phoenix/js/duck.js
+++ phoenix/static/phoenix/js/duck.js
@@ -1,10 +1,8 @@
 $(function() {
 
-    $("#deformField2").change(function() {
-      var dataset_name = $("#deformField2").val();
-      //console.log(dataset_name);
-      $.getJSON("/search/crai/"+dataset_name, function(data) {
-        console.log(data.result);
+    var search = function(value) {
+      $.getJSON("/search/crai/"+value, function(data) {
+        //console.log(data.result);
         dataset_name_tooltip = data.result.dataset_name_tooltip;
         file = data.result.file;
         file_tooltip = data.result.file_tooltip;
@@ -18,13 +16,14 @@ $(function() {
         $("#deformField4").val(variable_name);
         $("#item-deformField4").attr("title", variable_name_tooltip);
       });
+    };
+
+    $("#deformField2").change(function() {
+      var dataset_name = $("#deformField2").val();
+      //console.log(dataset_name);
+      search(dataset_name);
+      
     });
 
-    $("#deformField2").val("HadCRUT5");
-    $("#item-deformField2").attr("title", "");
-    $("#deformField3").val("https://www.metoffice.gov.uk/hadobs/hadcrut5/data/current/non-infilled/HadCRUT.5.0.1.0.anomalies.ensemble_mean.nc");
-    $("#deformField3").attr("title", "Enter a URL to your HadCRUT5 netcdf file.");
-    $("#item-deformField3").attr("title", "Enter a URL to your HadCRUT5 netcdf file.");
-    $("#deformField4").val("tas_mean");
-    $("#item-deformField4").attr("title", "tas_mean");
+    search("HadCRUT5");
   });
