a28ce12e0f8c6291859cfaa6d1026a174e56fe42
Pingu Carsti <ehbrecht@dkrz.de>
2023-08-16 16:56:50 +0200

    remove unsued monitor status and list views

--- phoenix/monitor/__init__.py
+++ phoenix/monitor/__init__.py
@@ -3,11 +3,6 @@ LOGGER = logging.getLogger(__name__)
 
 
 def includeme(config):
-    # settings = config.registry.settings
-
     # views
     config.add_route('monitor', '/monitor')
     config.add_route('job_details', '/monitor/details/{job_id}/{tab}')
-    config.add_route('job_status', '/monitor/status/{job_id}')
-    # actions
-    config.include('phoenix.monitor.views.actions')
